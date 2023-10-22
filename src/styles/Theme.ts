af5ada7009c126f3388663a5cb9da7cf6d23cd55
hyesung99 <4538asd@gmail.com>
2023-10-22 17:28:32 +0900

    style : theme파일 작성

---
+++ src/styles/Theme.ts
@@ -0,0 +1,42 @@
+import { ThemeProvider, keyframes } from '@emotion/react';
+
+const color = {
+  //codes for test
+  primary: '#03c75a',
+  secondary: '#ff5a03',
+  white: '#ffffff',
+  black: '#000000',
+  gray: '#f0f0f0',
+} as const;
+
+const componentStyle = {
+  input: {
+    borderRadius: '5px',
+  },
+};
+
+const keyframe = {
+  fadeIn: keyframes`
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }`,
+  fadeOut: keyframes`
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  `,
+};
+
+const Theme = {
+  color,
+  componentStyle,
+  keyframe,
+};
+
+export default Theme;
