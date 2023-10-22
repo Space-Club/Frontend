0660c336850ed6b7a838ee0bd0ad2f0e6a091fea
hyesung99 <4538asd@gmail.com>
2023-10-22 17:28:49 +0900

    style : theme, global style 적용

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import GlobalStyle from '@/styles/GlobalStyle';
+import Theme from '@/styles/Theme';
+
+import { Global, ThemeProvider } from '@emotion/react';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <Global styles={GlobalStyle} />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
