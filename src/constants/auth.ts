953a95a776a3f65bcc690ff7335d258a01428639
hyesung99 <4538asd@gmail.com>
2023-10-23 16:27:49 +0900

    feat : auth를 위한 상수 추가

---
+++ src/constants/auth.ts
@@ -0,0 +1,3 @@
+export const KAKAO_REST_API_KEY = 'd358bd786d22951d3c433fe2cbcd6689';
+export const KAKAO_REDIRECT_URI = 'http://localhost:5173/oauth/kakao';
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
