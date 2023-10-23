0a54425c305e40fe9093372e960160b23e862a5f
SongInJae <pan0663@naver.com>
2023-10-23 17:36:12 +0900

    feat: 라우팅 설정

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-const App = () => {
+const Layout = () => {
   return (
     <div>
       <Outlet />
@@ -9,4 +9,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Layout;
---
+++ src/pages/event/EventDetailPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EventDetailPage = () => {
+  return <div></div>;
+};
+
+export default EventDetailPage;
