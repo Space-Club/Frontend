7cfd60073a28f3a095b4102d78efecd9b74002af
hyesung99 <4538asd@gmail.com>
2023-10-24 17:35:05 +0900

    feat : kakaoLogin response User타입 설정

--- src/apis/auth/kakaoLogin.ts
+++ src/apis/auth/kakaoLogin.ts
@@ -1,8 +1,10 @@
-import { END_POINTS } from '@/constants/api';
+import { User } from '@/types/user';
+
+import { END_POINTS } from '@constants/api';
 
 import { axiosClient } from '../axiosClient';
 
 export const kakaoLogin = async (code: string) => {
-  const { data } = await axiosClient.post(END_POINTS.KAKAO_LOGIN, { code });
+  const { data } = await axiosClient.post<User>(END_POINTS.KAKAO_LOGIN, { code });
   return data;
 };
