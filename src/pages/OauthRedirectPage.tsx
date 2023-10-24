d04f8d0d709f5e70f11ced5ddea0df2b5e055b9c
hyesung99 <4538asd@gmail.com>
2023-10-24 17:35:30 +0900

    feat : 카카오 로그인 성공시 토큰 로컬스토리지 저장

--- src/pages/OauthRedirectPage.tsx
+++ src/pages/OauthRedirectPage.tsx
@@ -1,9 +1,20 @@
+import { kakaoLogin } from '@/apis/auth/kakaoLogin';
+
 import { useEffect } from 'react';
 
 const OauthRedirectPage = () => {
   const authCode = new URL(window.location.href).searchParams.get('code');
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (!authCode) {
+      throw new Error('인증 코드가 없습니다.');
+    }
+
+    kakaoLogin(authCode).then(({ accessToken }) => {
+      localStorage.setItem('accessToken', accessToken);
+      window.location.href = '/';
+    });
+  });
 
   return <div>OauthRedirectPage</div>;
 };
