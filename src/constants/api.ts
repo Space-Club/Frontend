c4c099f421f9198b33f52c40b830aa8a851117b1
hyesung99 <4538asd@gmail.com>
2023-10-24 16:36:22 +0900

    feat : api 상수 추가

---
+++ src/constants/api.ts
@@ -0,0 +1,9 @@
+const SPACECLUB_BASE_URL = 'https://spaceclub.site';
+
+const NETWORK_TIMEOUT = 10000;
+
+const END_POINTS = {
+  KAKAO_LOGIN: '/login/kakao',
+};
+
+export { SPACECLUB_BASE_URL, NETWORK_TIMEOUT, END_POINTS };
