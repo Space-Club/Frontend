c9e81b8a25545621348ecdc8df6e375591febeab
SongInJae <pan0663@naver.com>
2023-10-24 18:49:13 +0900

    Skrf 140 feat register function (#17)
    * feat: inputform 공통컴포넌트 수정
    
    * feat: inputform 공통컴포넌트 placeholder 속성 추가
    
    * 초기설정 페이지 기능 구현 초안 완료
    
    * design: 코드리뷰 반영
    
    * fix: 빌드 오류 수정

--- src/components/common/InputForm/InputForm.tsx
+++ src/components/common/InputForm/InputForm.tsx
@@ -1,16 +1,37 @@
+import { ChangeEvent, RefObject } from 'react';
+
 import { InputStyled, InputWrapper, LabelStyled } from './InputForm.style';
 
-interface InputForm {
+interface InputForm<T> {
   labelText: string;
-  labelId: string;
   inputType: 'date' | 'file' | 'number' | 'search' | 'tel' | 'text' | 'time';
+  placeholoder?: string;
+  inputRef?: RefObject<HTMLInputElement>;
+  value?: T;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputForm = ({ labelText, labelId, inputType, ...props }: InputForm) => {
+const InputForm = <T extends string | number | string[]>({
+  labelText,
+  inputType,
+  placeholoder,
+  inputRef,
+  value,
+  onChange,
+  ...props
+}: InputForm<T>) => {
   return (
     <InputWrapper>
-      <LabelStyled htmlFor={labelId}>{labelText}</LabelStyled>
-      <InputStyled id={labelId} type={inputType} {...props} />
+      <LabelStyled htmlFor={labelText}>{labelText}</LabelStyled>
+      <InputStyled
+        id={labelText}
+        type={inputType}
+        ref={inputRef}
+        placeholder={placeholoder}
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
     </InputWrapper>
   );
 };
