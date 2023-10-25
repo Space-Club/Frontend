3e25c342e92b0284042bab8d250d7cec4e8c7587
hyesung99 <4538asd@gmail.com>
2023-10-25 17:13:26 +0900

    SKRF-146 local storage 기능 추가 (#21)
    * feat : 스토리지 키 상수 저장
    
    * feat : 로컬스토리지 관련 함수 추가
    
    * fix : localStorage 에러 상황 변경

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,26 @@
+import { STORAGE_KEYS } from '@constants/storageKeys';
+
+type StorageKeys = keyof typeof STORAGE_KEYS;
+
+const setStorage = <T>(key: StorageKeys, value: T) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    localStorage.setItem(key, jsonValue);
+  } catch {
+    throw new Error('로컬 스토리지에 값을 저장하는 도중 오류가 발생했습니다.');
+  }
+};
+
+const getStorage = <T>(key: StorageKeys, defaultValue?: unknown): T => {
+  const jsonValue = localStorage.getItem(key);
+  if (!jsonValue && !defaultValue) {
+    throw new Error('로컬 스토리지에 존재하지 않는 값입니다.');
+  }
+  return jsonValue ? JSON.parse(jsonValue) : defaultValue;
+};
+
+const deleteStorage = (key: StorageKeys) => {
+  localStorage.removeItem(key);
+};
+
+export { getStorage, setStorage, deleteStorage };
