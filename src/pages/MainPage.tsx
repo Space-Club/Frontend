3e9c563dde7b9bc74e38d82dcea7bcf122d0d53f
colorkite10 <colorkite10@gmail.com>
2023-10-25 20:41:48 +0900

    design: 배너 컴포넌트 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,5 @@
-import Banner from '@/components/common/Banner/banner';
-
 const MainPage = () => {
-  return (
-    <div>
-      <Banner bannerImageSrc="https://picsum.photos/200/300" width={10} height={20}></Banner>
-    </div>
-  );
+  return <div>main</div>;
 };
 
 export default MainPage;
