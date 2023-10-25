190d87da28fdb37eae91310d47695c5cfe70e1e2
hyesung99 <4538asd@gmail.com>
2023-10-25 16:35:09 +0900

    fix : localStorage 에러 상황 변경

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -4,28 +4,23 @@ type StorageKeys = keyof typeof STORAGE_KEYS;
 
 const setStorage = <T>(key: StorageKeys, value: T) => {
   try {
-    const stringValue = JSON.stringify(value);
-    localStorage.setItem(key, stringValue);
+    const jsonValue = JSON.stringify(value);
+    localStorage.setItem(key, jsonValue);
   } catch {
     throw new Error('로컬 스토리지에 값을 저장하는 도중 오류가 발생했습니다.');
   }
 };
 
-const getStorage = <T>(key: StorageKeys): T => {
-  try {
-    const jsonValue = localStorage.getItem(key);
-    return jsonValue ? JSON.parse(jsonValue) : null;
-  } catch {
+const getStorage = <T>(key: StorageKeys, defaultValue?: unknown): T => {
+  const jsonValue = localStorage.getItem(key);
+  if (!jsonValue && !defaultValue) {
     throw new Error('로컬 스토리지에 존재하지 않는 값입니다.');
   }
+  return jsonValue ? JSON.parse(jsonValue) : defaultValue;
 };
 
 const deleteStorage = (key: StorageKeys) => {
-  try {
-    localStorage.removeItem(key);
-  } catch {
-    throw new Error('로컬 스토리지에 존재하지 않는 값입니다.');
-  }
+  localStorage.removeItem(key);
 };
 
 export { getStorage, setStorage, deleteStorage };
