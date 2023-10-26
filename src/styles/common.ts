f3c0ec7e7d9d6fa51e99adb48cf2ace68024edb2
hyesung99 <4538asd@gmail.com>
2023-10-26 22:00:39 +0900

    feat : Event공통 스타일 컴포넌트 추출 & text-overflow설정

---
+++ src/styles/common.ts
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+const EventTitleStyled = styled.div`
+  margin-bottom: 3%;
+  font-family: 'MainBold';
+  font-size: 1.5rem;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+`;
+
+const EventDateStyled = styled.div`
+  font-size: 1rem;
+`;
+
+const ClubNameStyled = styled.div`
+  font-family: 'MainRegular';
+  font-size: 0.8rem;
+`;
+
+const PlaceStyled = styled.div`
+  width: 100%;
+  color: grey;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  font-size: 1rem;
+`;
+
+export { EventTitleStyled, EventDateStyled, ClubNameStyled, PlaceStyled };
