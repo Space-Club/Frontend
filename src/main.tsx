8447ad8e6e68c1a34ce84996bef411473caa4e1c
hyesung99 <4538asd@gmail.com>
2023-10-26 14:58:27 +0900

    feat : QueryClient 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import GlobalStyle from '@styles/GlobalStyle';
 import Theme from '@styles/Theme';
 
@@ -14,11 +16,15 @@ if (import.meta.env.VITE_USER_NODE_ENV === 'development') {
   worker.start();
 }
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={Theme}>
-      <Global styles={GlobalStyle} />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={Theme}>
+        <Global styles={GlobalStyle} />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
