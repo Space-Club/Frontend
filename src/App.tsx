c2ba529dfdd495310ec54363ddd32da0e096203d
SongInJae <pan0663@naver.com>
2023-10-26 18:04:41 +0900

    fix: App.tsx react-queryclient 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
-
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
+    <div>
       <Outlet />
-    </QueryClientProvider>
+    </div>
   );
 };
 
