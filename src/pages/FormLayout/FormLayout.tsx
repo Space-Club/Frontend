80363b06ae1b1d6dc432b4492a111c20e6154ada
SongInJae <pan0663@naver.com>
2023-10-27 17:27:26 +0900

    Skrf 159 design form UI (#28)
    * feat: 폼 layout 라우팅 설정
    
    * feat: formlayout 구현
    
    * fix: export 방식 변경
    
    * fix: else문 추가
    
    * fix: route index 배포 에러 해결

---
+++ src/pages/FormLayout/FormLayout.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+
+import { Container, Content, Title } from './FormLayout.style';
+
+const FormLayout = () => {
+  const [title, setTitle] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+
+    if (path.endsWith('/choice')) {
+      setTitle('행사의 분류를 선택하세요.');
+    } else if (path.endsWith('/writeinfo')) {
+      setTitle('아래의 공연 정보를 입력해주세요.');
+    } else if (path.endsWith('/writeform')) {
+      setTitle('신청자에게 필요한 정보를 입력해주세요.');
+    } else {
+      throw new Error('옳지 못한 주소입니다. 다시 확인해주세요.');
+    }
+  }, [location.pathname]);
+
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Content>
+        <Outlet />
+      </Content>
+    </Container>
+  );
+};
+
+export default FormLayout;
