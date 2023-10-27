62f2f20440ace7a64c5ea2d9a0a148738de1ae5f
SongInJae <pan0663@naver.com>
2023-10-27 15:10:34 +0900

    fix: export 방식 변경

--- src/pages/FormLayout/FormLayout.style.ts
+++ src/pages/FormLayout/FormLayout.style.ts
@@ -1,13 +1,15 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
 `;
-export const Title = styled.div`
+const Title = styled.div`
   font-size: 2.5rem;
 `;
-export const Content = styled.div`
+const Content = styled.div`
   flex-grow: 1;
 `;
+
+export { Container, Title, Content };
