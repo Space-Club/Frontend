59c91fe8446710a1b083e99baf587b9148eb6e64
hyesung99 <4538asd@gmail.com>
2023-10-29 00:04:54 +0900

    feat : getMyEvent api res, req 타입 정의

--- src/apis/event/getMyEvent.ts
+++ src/apis/event/getMyEvent.ts
@@ -1,12 +1,11 @@
+import { END_POINTS } from '@/constants/api';
+import { GetMyEventRequest, GetMyEventResponse } from '@/types/event';
+
 import { axiosClientWithAuth } from '../axiosClient';
 
-interface GetMyEventProps {
-  pageNum: number;
-}
-
-const getMyEvent = async ({ pageNum }: GetMyEventProps) => {
-  const { data } = await axiosClientWithAuth.get(
-    `/events/myEvent?page=${pageNum}size=10&sort=id,startDate`,
+const getMyEvent = async ({ pageNumber }: GetMyEventRequest) => {
+  const { data } = await axiosClientWithAuth.get<GetMyEventResponse>(
+    `${END_POINTS.GET_MY_EVENT}?page=${pageNumber}size=10&sort=id,startDate`,
   );
   return data;
 };
