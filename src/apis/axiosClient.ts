748a111d312297d03f0d6e644b916764de2a0103
hyesung99 <4538asd@gmail.com>
2023-10-28 22:49:17 +0900

    feat : axiosClientWithAuth 추가

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -1,3 +1,5 @@
+import { getStorage } from '@/utils/localStorage';
+
 import axios from 'axios';
 
 import { NETWORK_TIMEOUT } from '@constants/api';
@@ -5,6 +7,15 @@ import { NETWORK_TIMEOUT } from '@constants/api';
 export const axiosClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+  timeout: NETWORK_TIMEOUT,
+});
+
+export const axiosClientWithAuth = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: getStorage('token'),
   },
   withCredentials: true,
   timeout: NETWORK_TIMEOUT,
