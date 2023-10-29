74d000b7bf3cf31a32536514a274dd6820c64073
hyesung99 <4538asd@gmail.com>
2023-10-29 17:03:30 +0900

    rename : useMyEventQuery 변수명 변경

--- src/hooks/query/event/useMyEventQuery.ts
+++ src/hooks/query/event/useMyEventQuery.ts
@@ -1,16 +1,13 @@
 import { getMyEvent } from '@/apis/event/getMyEvent';
+import { GetMyEventRequest } from '@/types/event';
 
 import { useQuery } from '@tanstack/react-query';
-
-interface UseMyEventProps {
-  pageNumber: number;
-}
 
 const QUERY_KEY = {
   MY_EVENT: 'MY_EVENT',
 };
 
-const useMyEvent = ({ pageNumber }: UseMyEventProps) => {
+const useMyEventQuery = ({ pageNumber }: GetMyEventRequest) => {
   const { data: myEvent } = useQuery({
     queryKey: [QUERY_KEY.MY_EVENT, pageNumber],
     queryFn: () => getMyEvent({ pageNumber }),
@@ -19,4 +16,4 @@ const useMyEvent = ({ pageNumber }: UseMyEventProps) => {
   return { myEvent };
 };
 
-export default useMyEvent;
+export default useMyEventQuery;
