dbd612f6025d5aaec2b13ac91fb0c1b1cb6aa8be
colorkite10 <colorkite10@gmail.com>
2023-10-29 17:32:02 +0900

    refactor: MainPage 스타일 파일 분리

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,27 +6,12 @@ import Tab from '@/components/common/Tab/Tab';
 import { TAB_CONSTANTS } from '@/constants/tab';
 import { TabContextProvider } from '@/context/TabContext';
 import { truncateText } from '@/utils/truncateText';
-import styled from '@emotion/styled';
 
-const ContentContainerStyled = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const BannerWrapperStyled = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 5% 0;
-`;
-
-const EventCardWrapperStyled = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  margin-bottom: 5%;
-`;
+import {
+  BannerWrapperStyled,
+  ContentContainerStyled,
+  EventCardWrapperStyled,
+} from './MainPage.style';
 
 const MainPage = () => {
   return (
