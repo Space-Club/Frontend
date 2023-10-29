85b4aaf14defb73fe610c6ec282aba7c7e2e66c9
colorkite10 <colorkite10@gmail.com>
2023-10-29 16:53:21 +0900

    design: truncate 함수 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/common/Header/Header';
 import Tab from '@/components/common/Tab/Tab';
 import { TAB_CONSTANTS } from '@/constants/tab';
 import { TabContextProvider } from '@/context/TabContext';
+import { truncateText } from '@/utils/truncateText';
 import styled from '@emotion/styled';
 
 const ContentContainerStyled = styled.div`
@@ -55,40 +56,40 @@ const MainPage = () => {
           <EventCard
             eventId={1}
             eventPoster="https://picsum.photos/153/213"
-            eventTitle="연어 전시회"
-            eventDate="2023/10/29, 15시 30분 dfad"
+            eventTitle={truncateText('연어 전시회1', 13)}
+            eventDate="2023/10/29"
             eventTime="15시 30분"
-            eventPlace="하천대강당"
-            clubName="불곰"
+            eventPlace={truncateText('코엑스', 10)}
+            clubName={truncateText('불곰', 12)}
           />
           <EventCard
             eventId={2}
             eventPoster="https://picsum.photos/153/213"
-            eventTitle="연어 전시회"
-            eventDate="2023/10/29"
-            eventTime="15시 30분"
-            eventPlace="하천대강당"
+            eventTitle={truncateText('가나다라마바사아자차카타파하', 13)}
+            eventDate="2023/10/30"
+            eventTime="15시"
+            eventPlace={truncateText('어디긴 네 마음이지 라는 본심을 숨기며', 10)}
             clubImageSrc="https://picsum.photos/200"
-            clubName="불곰"
+            clubName={truncateText('어제는 공연을 봤는데 오랜만에', 12)}
           />
           <EventCard
             eventId={3}
             eventPoster="https://picsum.photos/153/213"
-            eventTitle="연어 전시회가나다라알말"
-            eventDate="2023/10/29, 15시 30분ㅇㄻㄴㄻㄴㅇㄻㄴㄹㄹ"
-            eventTime="15시 30분"
-            eventPlace="하천대강당"
+            eventTitle={truncateText('추억의 8090: 그날의 우리', 13)}
+            eventDate="2023/10/31"
+            eventTime="16시"
+            eventPlace={truncateText('우리집', 10)}
             clubImageSrc="https://picsum.photos/200"
-            clubName="불곰열글자라면어떨까가나다"
+            clubName={truncateText('pinkfloyd', 12)}
           />
           <EventCard
             eventId={4}
             eventPoster="https://picsum.photos/153/213"
-            eventTitle="연어불곰합동 전시회"
-            eventDate="2023/10/29, 15시 30분ㅇㄻㄴㄻㄴㅇㄻㄴㄹㄹ"
-            eventTime="15시 30분"
-            eventPlace="하천대강당"
-            clubName="호엔하임"
+            eventTitle={truncateText('BornToBe', 13)}
+            eventDate="2023/10/32"
+            eventTime="32시 68분"
+            eventPlace={truncateText('화성', 10)}
+            clubName={truncateText('외존믿모', 12)}
           />
         </EventCardWrapperStyled>
       </ContentContainerStyled>
