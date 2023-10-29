b0a64356c2b903b73caf3b72c1d1cae4f9b31055
colorkite10 <colorkite10@gmail.com>
2023-10-29 13:32:19 +0900

    design: 메인페이지 레이아웃 디자인 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,33 @@
 import SearchInputForm from '@/components/SearchInputForm/SearchInputForm';
+import Banner from '@/components/common/Banner/Banner';
+import EventCard from '@/components/common/EventCard/EventCard';
 import Header from '@/components/common/Header/Header';
 import Tab from '@/components/common/Tab/Tab';
 import { TAB_CONSTANTS } from '@/constants/tab';
 import { TabContextProvider } from '@/context/TabContext';
+import styled from '@emotion/styled';
+
+const ContentContainerStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid;
+`;
+
+const BannerWrapperStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5% 0;
+`;
+
+const EventCardWrapperStyled = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 5%;
+  border: 1px solid;
+`;
 
 const MainPage = () => {
   return (
@@ -24,6 +49,45 @@ const MainPage = () => {
           ]}
         />
       </Header>
+      <ContentContainerStyled>
+        <BannerWrapperStyled>
+          <Banner width={35} height={20} />
+        </BannerWrapperStyled>
+        <EventCardWrapperStyled>
+          <EventCard
+            eventId={1}
+            eventPoster="https://picsum.photos/153/213"
+            eventTitle="연어 전시회"
+            eventDate="2023/10/29, 15시 30분ㅇㄻㄴㄻㄴㅇㄻㄴㄹㄹ"
+            eventPlace="하천대강당"
+            clubName="불곰"
+          />
+          <EventCard
+            eventId={2}
+            eventPoster="https://picsum.photos/153/213"
+            eventTitle="연어 전시회"
+            eventDate="2023/10/29, 15시 30분ㅇㄻㄴㄻㄴㅇㄻㄴㄹㄹ"
+            eventPlace="하천대강당"
+            clubName="불곰"
+          />
+          <EventCard
+            eventId={3}
+            eventPoster="https://picsum.photos/153/213"
+            eventTitle="연어 전시회"
+            eventDate="2023/10/29, 15시 30분ㅇㄻㄴㄻㄴㅇㄻㄴㄹㄹ"
+            eventPlace="하천대강당"
+            clubName="불곰"
+          />
+          <EventCard
+            eventId={4}
+            eventPoster="https://picsum.photos/153/213"
+            eventTitle="연어 전시회"
+            eventDate="2023/10/29, 15시 30분ㅇㄻㄴㄻㄴㅇㄻㄴㄹㄹ"
+            eventPlace="하천대강당"
+            clubName="불곰"
+          />
+        </EventCardWrapperStyled>
+      </ContentContainerStyled>
     </TabContextProvider>
   );
 };
