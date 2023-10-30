421a039d6b3bd5042cc8957f480eca441be8b64e
hyesung99 <4538asd@gmail.com>
2023-10-30 17:07:14 +0900

    fix : 빌드 오류 수정

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,6 @@
 import { LogoText, Message } from '@/constants/LoginPage';
 
-import KakaoLoginButton from '../../components/KakaoLoginButton/KakaoLoginButton';
+// import KakaoLoginButton from '../../components/KakaoLoginButton/KakaoLoginButton';
 import {
   LoginAreaStyled,
   LogoAreaStyled,
@@ -20,7 +20,7 @@ const LoginPage = () => {
       </LogoAreaStyled>
       <LoginAreaStyled>
         <TitleStyled>{Message.WELCOME}</TitleStyled>
-        <KakaoLoginButton />
+        {/* <KakaoLoginButton /> */}
       </LoginAreaStyled>
     </PageContainerStyled>
   );
