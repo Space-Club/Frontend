fcb6e2653a0a6766beda7e91fb19c6952cb2106a
hyesung99 <4538asd@gmail.com>
2023-10-30 14:56:30 +0900

    chore : usePostEventApplyMutation retry 설정

--- src/hooks/query/event/usePostEventApplyMutation.ts
+++ src/hooks/query/event/usePostEventApplyMutation.ts
@@ -5,8 +5,9 @@ import { useMutation } from '@tanstack/react-query';
 const usePostEventApplyMutation = () => {
   const { mutate: applyEvent } = useMutation({
     mutationFn: postEventApply,
-    onSuccess: () => {},
-    onError: () => {},
+    onSuccess: () => {}, //TODO: 신청 완료되었습니다 toast 띄우기
+    onError: () => {}, //TODO: 에러 발생했습니다 toast 띄우기
+    retry: 2,
   });
 
   return { applyEvent };
