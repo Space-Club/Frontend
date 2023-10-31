680292033701f8692ff34fca05f3a76634b089aa
SongInJae <pan0663@naver.com>
2023-10-31 16:18:28 +0900

    SKRF-155 feat event create UI (#33)
    * feat: CoiceEventPage 내용 작성
    
    * fix: Layout 컴포넌트 여백 오류 수정
    
    * feat: choiceEventPage ui 완성
    
    * style: styled 분리
    
    * fix: route 빌드 오류 수정
    
    * style: 라우팅 PATH 상수 사용
    
    * style: 라우팅 빌드 문제
    
    * style: 라우팅 빌드 문제 해결
    
    * fix: title margin-top 조절
    
    * fix: Card 내용 객체화
    
    * fix: 테스트를 위한 중간 저장
    
    * fix: Card 확대 애니메이션 적용
    
    * fix: route 오류 수정

--- src/pages/FormLayout/FormLayout.style.ts
+++ src/pages/FormLayout/FormLayout.style.ts
@@ -3,10 +3,10 @@ import styled from '@emotion/styled';
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
 `;
 const Title = styled.div`
   font-size: 2.5rem;
+  margin-top: 1rem;
 `;
 const Content = styled.div`
   flex-grow: 1;
