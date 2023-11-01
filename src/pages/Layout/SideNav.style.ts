fbf3104d99084df7fe54c7ec1d96caaf523c0baf
colorkite10 <colorkite10@gmail.com>
2023-11-01 17:23:26 +0900

    design: 사이드바 디자인 변경

--- src/pages/Layout/SideNav.style.ts
+++ src/pages/Layout/SideNav.style.ts
@@ -2,21 +2,21 @@ import Theme from '@/styles/Theme';
 import { css } from '@emotion/css';
 import styled from '@emotion/styled';
 
-export const Container = styled.div`
+const SidebarContainer = styled.div`
   position: fixed;
-  width: 7rem;
+  width: 5.7rem;
   height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 1rem;
-  padding-bottom: 1rem;
+  padding-bottom: 2rem;
   box-sizing: border-box;
   background: ${Theme.color.logo_2};
   color: ${Theme.color.gray};
 `;
 
-export const AvatarGroup = styled.div`
+const ClubWrapper = styled.div`
   overflow: auto;
   flex-grow: 1;
   display: flex;
@@ -29,9 +29,51 @@ export const AvatarGroup = styled.div`
     padding-top: 1rem;
   }
 `;
-export const iconStyle = css`
-  display: block;
-  width: 2rem;
-  height: 2rem;
+
+const CreateClubButtonStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4rem;
+  height: 1.625rem;
+  border-radius: 0.75rem;
+  background: ${Theme.color.sidebarBackground};
+  box-shadow: 0px 1px 4px 0px ${Theme.color.shadow};
+  font-size: ${Theme.fontSize.tagText};
   cursor: pointer;
+
+  position: relative;
+
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 0.3s ease-in-out;
+  }
+
+  &::before {
+    content: '클럽 생성';
+    position: absolute;
+    top: -100%;
+    background: ${Theme.color.shadow};
+    color: ${Theme.color.tWhiteGrey};
+    padding: 0.3rem;
+    border-radius: 0.25rem;
+    opacity: 0;
+    transition: opacity 0.2s ease-in-out;
+  }
+
+  &:hover::before {
+    opacity: 1;
+  }
 `;
+
+const iconStyle = css`
+  display: block;
+  width: 3rem;
+  height: 3rem;
+  cursor: pointer;
+  fill: rgba(239, 239, 244, 0.3);
+
+  filter: drop-shadow(2px 3px 1px ${Theme.color.shadow});
+`;
+
+export { SidebarContainer, ClubWrapper, CreateClubButtonStyled, iconStyle };
--- src/pages/Layout/SideNav.tsx
+++ src/pages/Layout/SideNav.tsx
@@ -3,12 +3,13 @@ import SideBarMyProfile from '@/components/SideBarMyProfile/SideBarMyProfile';
 import Avatar from '@/components/common/Avatar/Avatar';
 import { PATH } from '@/constants/path';
 
-import { FaBell, FaHome, FaPlusCircle } from 'react-icons/fa';
+import { FaPlusCircle } from 'react-icons/fa';
+import { IoMdHome, IoMdNotifications } from 'react-icons/io';
 import { Link, useNavigate } from 'react-router-dom';
 
 import { useQuery } from '@tanstack/react-query';
 
-import { AvatarGroup, Container, iconStyle } from './SideNav.style';
+import { ClubWrapper, CreateClubButtonStyled, SidebarContainer, iconStyle } from './SideNav.style';
 
 const SideNav = () => {
   const { data: clubs } = useQuery(['myclubs', 'userid'], () => getMyClub({ id: '1' }), {
@@ -17,21 +18,23 @@ const SideNav = () => {
   const navigate = useNavigate();
 
   return (
-    <Container>
-      <AvatarGroup>
+    <SidebarContainer>
+      <ClubWrapper>
         {clubs?.map((club) => (
           <Link to={`/club/home/${club.id}`}>
             <Avatar key={club.id} avatarSize="normal" profileImageSrc={club.src}></Avatar>
           </Link>
         ))}
-      </AvatarGroup>
-      <FaPlusCircle className={iconStyle} onClick={() => navigate(`${PATH.CLUB.CREATE}`)} />
-      <FaHome className={iconStyle} onClick={() => navigate('/')} />
-      <FaBell className={iconStyle} onClick={() => alert('알림페이지 준비 중')} />
+      </ClubWrapper>
+      <CreateClubButtonStyled>
+        <FaPlusCircle size={'1rem'} onClick={() => navigate(`${PATH.CLUB.CREATE}`)} />
+      </CreateClubButtonStyled>
+      <IoMdHome className={iconStyle} onClick={() => navigate('/')} />
+      <IoMdNotifications className={iconStyle} onClick={() => alert('알림페이지 준비 중')} />
       <Link to={`/profile`}>
         <SideBarMyProfile />
       </Link>
-    </Container>
+    </SidebarContainer>
   );
 };
 
