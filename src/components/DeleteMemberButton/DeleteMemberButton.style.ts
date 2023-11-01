ebde5bc103892b7fc60b98cead1f0d24e2349096
hyesung99 <4538asd@gmail.com>
2023-11-01 17:10:45 +0900

    feat : DeleteMemberButton 컴포넌트 추가

---
+++ src/components/DeleteMemberButton/DeleteMemberButton.style.ts
@@ -0,0 +1,16 @@
+import Theme from '@/styles/Theme';
+import styled from '@emotion/styled';
+
+const DeleteMemberButtonStyled = styled.button`
+  width: 1.8125rem;
+  height: 1.25rem;
+  line-height: 1.4rem;
+  border-radius: 0.3125rem;
+  font-size: ${Theme.fontSize.tagText};
+  background-color: ${Theme.color.tWhiteGrey};
+  border: none;
+  cursor: pointer;
+  outline: none;
+`;
+
+export { DeleteMemberButtonStyled };
