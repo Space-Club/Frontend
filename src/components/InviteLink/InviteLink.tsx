2f652a28f4151944537645084025e21731d747a8
SongInJae <pan0663@naver.com>
2023-11-01 17:22:49 +0900

    fix: link 받아오는 방식 변경 및 링크 유효시간 상수화

--- src/components/InviteLink/InviteLink.tsx
+++ src/components/InviteLink/InviteLink.tsx
@@ -1,6 +1,5 @@
 import getInviteLink from '@/apis/club/getInviteLink';
-
-import { useState } from 'react';
+import { INVITE_LINK } from '@/constants/club';
 
 import { useQuery } from '@tanstack/react-query';
 
@@ -15,22 +14,21 @@ import {
 } from './InviteLink.style';
 
 const InviteLink = () => {
-  const { refetch } = useQuery(['inviteLink'], () => getInviteLink(), {
+  const { data, refetch } = useQuery(['inviteLink'], () => getInviteLink(), {
     enabled: false,
-    onSuccess: (fetchData) => {
-      setLink(fetchData.link);
+    initialData: {
+      link: '',
     },
   });
-  const [link, setLink] = useState('');
 
   return (
     <InviteLinkContainer>
       <TitleWrapper>
         <Title>초대 링크</Title>
-        <SubTitle>링크의 유효기간은 48시간입니다.</SubTitle>
+        <SubTitle>링크의 유효기간은 {INVITE_LINK.VALID_TIME}시간입니다.</SubTitle>
       </TitleWrapper>
       <InputWrapper>
-        <ReadonlyInput value={link} readOnly />
+        <ReadonlyInput value={data.link} readOnly />
         <SubmitButton onClick={() => refetch()}>생성</SubmitButton>
       </InputWrapper>
     </InviteLinkContainer>
