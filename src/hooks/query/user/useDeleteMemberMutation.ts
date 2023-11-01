b43f9ac5c99f81c4734577451295ee6ae3919202
hyesung99 <4538asd@gmail.com>
2023-11-01 17:11:10 +0900

    chore : 주석 변경

--- src/hooks/query/user/useDeleteMemberMutation.ts
+++ src/hooks/query/user/useDeleteMemberMutation.ts
@@ -5,15 +5,15 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 const useDeleteMemberMutation = () => {
   const queryClient = useQueryClient();
 
-  const { mutate: withdrawMember, ...lefts } = useMutation({
+  const { mutate: withdrawMember, ...others } = useMutation({
     mutationFn: deleteMember,
     onSuccess: () => {
-      queryClient.invalidateQueries(['clubMembers']); //TODO: clubMembers 캐시 데이터stale시켜서 refetch
+      queryClient.invalidateQueries(['clubMembers']);
     },
     onError: () => {}, //TODO: 에러 발생했습니다 toast 띄우기
   });
 
-  return { withdrawMember, ...lefts };
+  return { withdrawMember, ...others };
 };
 
 export default useDeleteMemberMutation;
