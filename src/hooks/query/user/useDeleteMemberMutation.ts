f0526520ede003a05b420671f0475c26e6e9279e
hyesung99 <4538asd@gmail.com>
2023-11-01 16:59:20 +0900

    feat : useDeleteMemberMutation invalidate추가

--- src/hooks/query/user/useDeleteMemberMutation.ts
+++ src/hooks/query/user/useDeleteMemberMutation.ts
@@ -1,14 +1,19 @@
 import deleteMember from '@/apis/users/deleteMember';
 
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const useDeleteMemberMutation = () => {
-  const { mutate: withdrawMember } = useMutation({
+  const queryClient = useQueryClient();
+
+  const { mutate: withdrawMember, ...lefts } = useMutation({
     mutationFn: deleteMember,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['clubMembers']); //TODO: clubMembers 캐시 데이터stale시켜서 refetch
+    },
     onError: () => {}, //TODO: 에러 발생했습니다 toast 띄우기
   });
 
-  return { withdrawMember };
+  return { withdrawMember, ...lefts };
 };
 
 export default useDeleteMemberMutation;
