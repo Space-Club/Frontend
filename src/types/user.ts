5ee68c05d45f7e16036321e881937b41b8576f1d
hyesung99 <4538asd@gmail.com>
2023-11-02 22:05:06 +0900

    feat : getUser api 추가

--- src/types/user.ts
+++ src/types/user.ts
@@ -2,6 +2,13 @@ interface User {
   token: string;
 }
 
+interface ClubMember {
+  id: string;
+  name: string;
+  image: string;
+  role: string;
+}
+
 type AvatarSize = 'normal' | 'large' | 'small' | 'medium';
 
-export { User, AvatarSize };
+export { User, AvatarSize, ClubMember };
