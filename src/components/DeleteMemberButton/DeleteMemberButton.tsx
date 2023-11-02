0626ac7c235c2e31766d85b59c347486ce2a2fb9
hyesung99 <4538asd@gmail.com>
2023-11-02 23:53:10 +0900

    styled : 탈퇴 버튼 색깔 변경

--- src/components/DeleteMemberButton/DeleteMemberButton.tsx
+++ src/components/DeleteMemberButton/DeleteMemberButton.tsx
@@ -10,6 +10,7 @@ interface DeleteMemberButtonProps extends React.HTMLAttributes<HTMLButtonElement
 const DeleteMemberButton = ({ memberId, clubId, children, ...props }: DeleteMemberButtonProps) => {
   const { withdrawMember } = useDeleteMemberMutation();
 
+  //TODO: 탈퇴 버튼 클릭 시, confirm 모달 띄우기
   return (
     <DeleteMemberButtonStyled onClick={() => withdrawMember({ memberId, clubId })} {...props}>
       탈퇴{children}
