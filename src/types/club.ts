58920aba7972845d278378b72141aae6acd92d38
hyesung99 <4538asd@gmail.com>
2023-11-03 16:31:07 +0900

    feat : Club타입 지정

--- src/types/club.ts
+++ src/types/club.ts
@@ -1,6 +1,13 @@
-interface CreateClubFormValue {
+interface Club {
   name: string;
+  image: string;
   info: string;
+  memberCount: 1;
+  background: string;
+  inviteCode: string;
+}
+
+interface CreateClubFormValue extends Pick<Club, 'name' | 'info'> {
   owner: string;
   image: FileList | null;
 }
@@ -9,4 +16,4 @@ interface getInviteLinkResponse {
   link: 'string'; // TODO 명세서 나올시 수정 필요;
 }
 
-export { getInviteLinkResponse, CreateClubFormValue };
+export { getInviteLinkResponse, CreateClubFormValue, Club };
