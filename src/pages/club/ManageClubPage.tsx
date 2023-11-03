185d5eb4f949b9eaf40f7280c9e7bac6bf6aeae4
hyesung99 <4538asd@gmail.com>
2023-11-03 15:07:56 +0900

    SKRF-195 feat: DropDown 컴포넌트 (#49)
    * feat : patchMemberRole api 추가
    
    * feat : usePatchmemberRoleMutation 커스텀 훅 추가
    
    * feat : dropDown 컴포넌트 추가
    
    * feat : dropDown 기본값 정의
    
    * chore : 카카오 로그인 엔드포인트 변경
    
    * feat : 멤버 role 상수 구조 변경
    
    * refactor : 요청 타입에서 role타입 구체화
    
    * feat : 페이지에 컴포넌트 연결

--- src/pages/club/ManageClubPage.tsx
+++ src/pages/club/ManageClubPage.tsx
@@ -1,9 +1,33 @@
 import InviteLink from '@/components/InviteLink/InviteLink';
+import DropDown from '@/components/common/DropDown/DropDown';
+import { MEMBER_ROLE_DROPDOWN_OPTIONS } from '@/constants/user';
+import usePatchMemberRoleMutation from '@/hooks/query/user/usePatchMemberRoleMutation';
+import { MemberRole } from '@/types/user';
 
-import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ManageClubPage = () => {
-  return <InviteLink />;
+  const { pathname } = useLocation();
+  const { changeMemberRole } = usePatchMemberRoleMutation();
+
+  const clubId = pathname.split('/').pop();
+
+  if (!clubId) {
+    throw new Error('path에서 clubId를 가져올 수 없습니다.');
+  }
+
+  return (
+    <>
+      <DropDown
+        options={MEMBER_ROLE_DROPDOWN_OPTIONS}
+        selectedValue={MEMBER_ROLE_DROPDOWN_OPTIONS[0].value ?? ''} //TODO 실제 멤버 ROLE 값으로 변경
+        onChange={(event) => {
+          changeMemberRole({ memberId: '1', clubId, role: event.target.value as MemberRole }); //TODO 실제 멤버 ID로 변경
+        }}
+      />
+      <InviteLink />;
+    </>
+  );
 };
 
 export default ManageClubPage;
