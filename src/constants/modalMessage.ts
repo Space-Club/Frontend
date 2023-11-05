283e0e78a0623f85260a8b4dbf87ee69220742cc
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2023-11-05 18:23:36 +0900

    SKRF-184 feat: 모달 컴포넌트 구현 (#53)
    * design: 확인 모달 디자인 구현
    
    * design: Confirm Modal 디자인 구현
    
    * design: 모달 렌더링 위치 변경
    
    * feat: useModal 커스텀 훅 구현 및 테스트
    
    * refactor: 모달 상수 분리

---
+++ src/constants/modalMessage.ts
@@ -0,0 +1,12 @@
+const MODAL_TEXT = {
+  UNDONE: '저장되지 않은 수정사항이 있습니다.',
+  EVENT_APPLY: '참여 신청을 하시겠습니까?',
+};
+
+const MODAL_BUTTON_TEXT = {
+  CONFIRM: '확인',
+  CANCEL: '취소',
+  EXIT: '나가기',
+};
+
+export { MODAL_TEXT, MODAL_BUTTON_TEXT };
