fe893877257edfd6a8838c35ef364cc11519dff1
colorkite10 <colorkite10@gmail.com>
2023-11-05 18:46:50 +0900

    fix: Avatar컴포넌트 props타입 변경

--- src/components/common/Avatar/Avatar.tsx
+++ src/components/common/Avatar/Avatar.tsx
@@ -14,7 +14,7 @@ import {
 
 interface AvatarProps extends React.HTMLAttributes<HTMLDivElement> {
   avatarSize: AvatarSize;
-  profileImageSrc?: string;
+  profileImageSrc?: string | null;
   isEditable?: boolean;
   isClub?: boolean;
 }
