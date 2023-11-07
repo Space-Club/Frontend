9d4ea6aa3be505a33bd6c5de941789bed83a9d11
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2023-11-07 18:02:46 +0900

    SKRF-193 feat: 사이드바 유저 프로필 이미지 api 테스트 완료 (#60)
    * feat: 사이드바에서 유저 프로필 이미지 가져오기 api 구현
    
    * feat: 사이드바 유저 프로필 이미지 불러오기 api 변경사항 적용 및 테스트 완료
    
    * fix: 빌드테스트 오류 해결
    
    * fix: 충돌해결
    
    * fix: 빌드테스트 오류 해결

--- src/pages/Layout/SideNav.tsx
+++ src/pages/Layout/SideNav.tsx
@@ -2,6 +2,7 @@ import SideBarMyProfile from '@/components/SideBarMyProfile/SideBarMyProfile';
 import Avatar from '@/components/common/Avatar/Avatar';
 import { PATH } from '@/constants/path';
 import useClubs from '@/hooks/query/club/useClubs';
+import useUserImageQuery from '@/hooks/query/user/useUserImageQuery';
 
 import { FaPlusCircle } from 'react-icons/fa';
 import { IoMdHome, IoMdNotifications } from 'react-icons/io';
@@ -10,13 +11,15 @@ import { Link, useNavigate } from 'react-router-dom';
 import { ClubWrapper, CreateClubButtonStyled, SidebarContainer, iconStyle } from './SideNav.style';
 
 const SideNav = () => {
-  const { myClubs: myClubs } = useClubs();
+  const { clubs } = useClubs();
+  const { userImage } = useUserImageQuery();
+
   const navigate = useNavigate();
 
   return (
     <SidebarContainer>
       <ClubWrapper>
-        {myClubs?.map((club) => (
+        {clubs?.map((club) => (
           <Link to={`/club/${club.id}/home`}>
             <Avatar key={club.id} avatarSize="normal" profileImageSrc={club.logoImageUrl}></Avatar>
           </Link>
@@ -28,7 +31,7 @@ const SideNav = () => {
       <IoMdHome className={iconStyle} onClick={() => navigate('/')} />
       <IoMdNotifications className={iconStyle} onClick={() => alert('알림페이지 준비 중')} />
       <Link to={`/profile`}>
-        <SideBarMyProfile />
+        <SideBarMyProfile profileImageUrl={userImage?.profileImageUrl} />
       </Link>
     </SidebarContainer>
   );
