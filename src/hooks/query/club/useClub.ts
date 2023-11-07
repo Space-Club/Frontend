0a6bf04b3c0921b0a87b7dfd66addbb409f166d1
colorkite10 <colorkite10@gmail.com>
2023-11-07 17:53:04 +0900

    feat: 클럽 생성 시 클럽 목록 캐시 무효화

--- src/hooks/query/club/useClub.ts
+++ src/hooks/query/club/useClub.ts
@@ -2,14 +2,18 @@ import postCreateClub from '@/apis/club/postCreateClub';
 
 import { useNavigate } from 'react-router-dom';
 
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { QUERY_KEY } from './useClubs';
 
 export const useClub = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: createClub, isLoading } = useMutation(postCreateClub, {
     onSuccess: (data: string) => {
       //#TODO: 클럽 생성 되었다는 토스트 메시지와 함께 이동
+      queryClient.invalidateQueries([QUERY_KEY.MY_CLUB]);
       const clubId = data.split('/').pop();
       navigate(`/club/${clubId}/home`);
     },
