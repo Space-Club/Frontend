88f2c27b9cb96ed85cbe0b495d9a15bcde078e5b
SongInJae <pan0663@naver.com>
2023-11-07 15:31:26 +0900

    fix: 랜더링 최적화

--- src/components/common/Pagination/Pagination.tsx
+++ src/components/common/Pagination/Pagination.tsx
@@ -1,16 +1,16 @@
-import { Dispatch, SetStateAction } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 import { PageButton, PaginationWrapper } from './Pagination.style';
 
 interface Pagination {
-  page: number;
-  setPage: Dispatch<SetStateAction<number>>;
   total: number;
   limit: number;
   pageLength?: number;
+  onChangePage: (page: number) => void;
 }
 
-const Pagination = ({ page, setPage, total, limit, pageLength = 10 }: Pagination) => {
+const Pagination = ({ total, limit, pageLength = 10, onChangePage }: Pagination) => {
+  const [page, setPage] = useState(0);
   const totalPageNum = Math.ceil(total / limit);
 
   const currentPageSize = Math.floor(page / pageLength);
@@ -18,32 +18,55 @@ const Pagination = ({ page, setPage, total, limit, pageLength = 10 }: Pagination
   const startPage = currentPageSize * pageLength + 1;
   const endPage = Math.min((currentPageSize + 1) * pageLength, totalPageNum);
 
-  const pageNumbers = Array.from(
-    { length: endPage - startPage + 1 },
-    (_, index) => startPage + index,
+  const pageNumbers = useMemo(() => {
+    return Array.from({ length: endPage - startPage + 1 }, (_, index) => startPage + index);
+  }, [startPage, endPage]);
+
+  const handleDoublePrevClick = useCallback(() => {
+    onChangePage(0);
+    setPage(0);
+  }, [onChangePage]);
+  const handlePrevClick = useCallback(() => {
+    onChangePage(page - 1);
+    setPage(page - 1);
+  }, [onChangePage, page]);
+  const handlePageClick = useCallback(
+    (pageNumber: number) => {
+      onChangePage(pageNumber - 1);
+      setPage(pageNumber - 1);
+    },
+    [onChangePage],
   );
+  const handleNextClick = useCallback(() => {
+    onChangePage(page + 1);
+    setPage(page + 1);
+  }, [onChangePage, page]);
+  const handleDoubleNextClick = useCallback(() => {
+    onChangePage(totalPageNum - 1);
+    setPage(totalPageNum - 1);
+  }, [onChangePage, totalPageNum]);
 
   return (
     <PaginationWrapper>
-      <PageButton onClick={() => setPage(0)} disabled={page === 0}>
+      <PageButton onClick={handleDoublePrevClick} disabled={page === 0}>
         &lt;&lt;
       </PageButton>
-      <PageButton onClick={() => setPage(page - 1)} disabled={page === 0}>
+      <PageButton onClick={handlePrevClick} disabled={page === 0}>
         &lt;
       </PageButton>
       {pageNumbers.map((value) => (
         <PageButton
           key={value - 1}
           aria-current={page + 1 === value ? 'page' : undefined}
-          onClick={() => setPage(value - 1)}
+          onClick={() => handlePageClick(value)}
         >
           {value}
         </PageButton>
       ))}
-      <PageButton onClick={() => setPage(page + 1)} disabled={page === totalPageNum - 1}>
+      <PageButton onClick={handleNextClick} disabled={page === totalPageNum - 1}>
         &gt;
       </PageButton>
-      <PageButton onClick={() => setPage(totalPageNum - 1)} disabled={page === totalPageNum - 1}>
+      <PageButton onClick={handleDoubleNextClick} disabled={page === totalPageNum - 1}>
         &gt;&gt;
       </PageButton>
     </PaginationWrapper>
