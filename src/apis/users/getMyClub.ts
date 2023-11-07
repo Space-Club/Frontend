ddcce9c55366a36e44debe8acda926e4967c0880
colorkite10 <colorkite10@gmail.com>
2023-11-07 10:53:01 +0900

    rename: 클럽 목록 조회 api endpoint 변경

--- src/apis/users/getMyClub.ts
+++ src/apis/users/getMyClub.ts
@@ -10,6 +10,7 @@ interface GetMyClub {
 
 const getMyClubs = async () => {
   const { data } = await axiosClientWithAuth.get<GetMyClub[]>(`${END_POINTS.MY_CLUB}/1`);
+  //#TODO: path parameter에서 토큰으로 변경하기
   console.log(data);
   return data;
 };
