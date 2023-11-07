529535d86ad9baea5684821263559b6114f331b6
hyesung99 <4538asd@gmail.com>
2023-11-07 19:01:36 +0900

    SKRF-237 fix : redirect url 변경하기 (#65)

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,7 +1,4 @@
 export const KAKAO_REST_API_KEY = 'd239a34faadbf264fafdf0eeacc7e0fd';
-export const KAKAO_REDIRECT_URI =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5173/oauths/kakao/callback'
-    : 'https://spaceclub.vercel.app/oauths/kakao/callback';
+export const KAKAO_REDIRECT_URI = 'https://spaceclub.vercel.app/oauths/kakao/callback';
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
