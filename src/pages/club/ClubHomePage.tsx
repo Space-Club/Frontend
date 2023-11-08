1e50bce19932096ca9959edc33957e80048031be
hyesung99 <4538asd@gmail.com>
2023-11-08 17:10:59 +0900

    refactor : 클럽 배너 id prop으로 변경

--- src/pages/club/ClubHomePage.tsx
+++ src/pages/club/ClubHomePage.tsx
@@ -1,9 +1,16 @@
 import ClubBanner from '@/components/common/ClubBanner/ClubBanner';
+import Header from '@/components/common/Header/Header';
+
+import { useParams } from 'react-router-dom';
 
 const ClubHomePage = () => {
+  const { clubId } = useParams();
+  if (!clubId) throw new Error('클럽 ID를 찾을 수 없습니다');
+
   return (
     <>
-      <ClubBanner />
+      <Header></Header>
+      <ClubBanner clubId={clubId} />
     </>
   );
 };
