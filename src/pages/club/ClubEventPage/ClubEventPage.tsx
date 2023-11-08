7307466ae1aeb97e5580fd1343306260a8c4bec9
colorkite10 <colorkite10@gmail.com>
2023-11-09 02:10:20 +0900

    fix: 빌드테스트 오류 해결

--- src/pages/club/ClubEventPage/ClubEventPage.tsx
+++ src/pages/club/ClubEventPage/ClubEventPage.tsx
@@ -2,9 +2,8 @@ import ActiveButton from '@/components/ActiveButton/ActiveButton';
 import EventCard from '@/components/common/EventCard/EventCard';
 import Header from '@/components/common/Header/Header';
 import Tab from '@/components/common/Tab/Tab';
-import { CREATE_EVENT } from '@/constants/club';
+import { CLUB_TABS, CREATE_EVENT } from '@/constants/club';
 import { PATH } from '@/constants/path';
-import { TAB_CONSTANTS } from '@/constants/tab';
 import { TabContextProvider } from '@/context/TabContext';
 import useClubEventsQuery from '@/hooks/query/club/useClubEventsQuery';
 
@@ -20,13 +19,7 @@ const ClubEventPage = () => {
     <TabContextProvider>
       <Header>
         <HeaderElementWrapper>
-          <Tab
-            tabItems={[
-              { title: `${TAB_CONSTANTS.CLUB_HOME}` },
-              { title: `${TAB_CONSTANTS.CLUB_PERFORMANCE}` },
-              { title: `${TAB_CONSTANTS.CLUB_MANAGEMENT}` },
-            ]}
-          />
+          <Tab tabItems={CLUB_TABS} />
         </HeaderElementWrapper>
       </Header>
       <EventsContainer>
