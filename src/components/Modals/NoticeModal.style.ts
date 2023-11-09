5564d4fddb2f04c28a830b502193ecbb1d453175
hyesung99 <4538asd@gmail.com>
2023-11-09 19:56:33 +0900

    SKRF-261 feat : 클럽 공지사항, 모달 (#73)
    * feat : 클럽 공지사항 컴포넌트 추가
    
    * feat : 클럽 공지사항 모달 추가
    
    * feat : 공지사항 권한 추가
    
    * chore : 테스트용 속성 수정
    
    * feat : 삭제 버튼 추가

---
+++ src/components/Modals/NoticeModal.style.ts
@@ -0,0 +1,51 @@
+import { PurpleButton } from '@/pages/event/EventDetailPage/EventDetailPage.style';
+import Theme from '@/styles/Theme';
+import { ellipsisTitle } from '@/styles/common';
+import styled from '@emotion/styled';
+
+import { ModalBaseContainer } from './Modal.style';
+
+const NoticeModalContainer = styled(ModalBaseContainer)`
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  transform: translate(-50%, -50%);
+  border-radius: 1.6rem;
+`;
+
+const NoticeModalHeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 4rem;
+  padding: 0 1.2rem;
+`;
+
+const NoticeCloseButtonStyled = styled.div`
+  cursor: pointer;
+`;
+
+const NoticeTitleStyled = styled(ellipsisTitle)`
+  flex: 1;
+  font-size: ${Theme.fontSize.smallTitle};
+  margin-left: 0.7rem;
+`;
+
+const NoticeButtonStyled = styled(PurpleButton)`
+  height: 2rem;
+  padding: 0 0.6rem;
+  margin-left: 0.3rem;
+`;
+
+const NoticeContentWrapper = styled.div`
+  padding: 1.5rem 2.6rem;
+  background-color: ${Theme.color.white};
+`;
+
+export {
+  NoticeModalHeaderWrapper,
+  NoticeContentWrapper,
+  NoticeModalContainer,
+  NoticeTitleStyled,
+  NoticeCloseButtonStyled,
+  NoticeButtonStyled,
+};
