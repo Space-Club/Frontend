8a7fbb4144cb77d4043609daeaa6f521e099ae2c
hyesung99 <4538asd@gmail.com>
2023-11-10 21:26:42 +0900

    feat : 폼을 위한 상수 추가

---
+++ src/constants/form.ts
@@ -0,0 +1,65 @@
+const name = {
+  name: '이름',
+};
+const phoneNumber = {
+  name: '연락처',
+};
+const depositor = {
+  name: '입금자명',
+};
+const numberOfPeople = {
+  name: '인원 수',
+};
+const age = {
+  name: '나이',
+};
+const gender = {
+  name: '성별',
+};
+const major = {
+  name: '학교/과',
+};
+const mbti = {
+  name: 'MBTI',
+};
+const introduce = {
+  name: '자기소개',
+};
+const motivation = {
+  name: '지원동기',
+};
+
+const PERFORMANCE_FORM_OPTION = {
+  name,
+  phoneNumber,
+  numberOfPeople,
+  depositor,
+};
+
+const PROMOTION_FORM_OPTION = {
+  name,
+  phoneNumber,
+};
+
+const RECRUITMENT_FORM_OPTION = {
+  name,
+  phoneNumber,
+  age,
+  gender,
+  major,
+  mbti,
+  introduce,
+  motivation,
+};
+
+const CLUB_SCHEDULE_FORM_OPTION = {
+  name,
+  phoneNumber,
+};
+
+export {
+  PERFORMANCE_FORM_OPTION,
+  PROMOTION_FORM_OPTION,
+  RECRUITMENT_FORM_OPTION,
+  CLUB_SCHEDULE_FORM_OPTION,
+};
