ec50c4c951a4f771a37f26bbcbcd30f825d46531
colorkite10 <colorkite10@gmail.com>
2023-11-12 00:37:59 +0900

    design:디자인 중

---
+++ src/pages/club/InvitePage/InvitePage.tsx
@@ -0,0 +1,11 @@
+import ClubInfo from '@/components/common/ClubInfo/ClubInfo';
+
+const InvitePage = () => {
+  return (
+    <div>
+      <ClubInfo name="연사모" info="연어를 사랑하는 사람들의 모임" memberCount="4" />
+    </div>
+  );
+};
+
+export default InvitePage;
