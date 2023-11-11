75676a551d79fbdbb9ce3db2bde8e729fa4cabe1
colorkite10 <colorkite10@gmail.com>
2023-11-11 22:49:11 +0900

    feat: 로그인 하지 않은 유저일 시 프로필 대신 로그인 버튼 보여줌

---
+++ src/components/LoginButton/LoginButton.style.ts
@@ -0,0 +1,26 @@
+import Theme from '@/styles/Theme';
+import styled from '@emotion/styled';
+
+const LoginButtonStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4rem;
+  height: 1.6rem;
+  border-radius: 0.3rem;
+  background: rgba(239, 239, 244, 0.2);
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
+
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 0.2s ease-in-out;
+  }
+`;
+
+const TextStyled = styled.span`
+  word-break: keep-all;
+  font-size: ${Theme.fontSize.tagText};
+  color: white;
+`;
+
+export { LoginButtonStyled, TextStyled };
---
+++ src/components/LoginButton/LoginButton.tsx
@@ -0,0 +1,11 @@
+import { LoginButtonStyled, TextStyled } from './LoginButton.style';
+
+const LoginButton = () => {
+  return (
+    <LoginButtonStyled>
+      <TextStyled>로그인 하기</TextStyled>
+    </LoginButtonStyled>
+  );
+};
+
+export default LoginButton;
