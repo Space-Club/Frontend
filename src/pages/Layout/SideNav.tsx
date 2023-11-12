f7a50dfef5999c62ea9ad78be8fb8165ed3e721b
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2023-11-13 00:44:50 +0900

    SKRF-269 feat: 로그인 하지 않은 유저일 시 프로필 대신 로그인 버튼 보여줌 (#78)
    * feat: 로그인 하지 않은 유저일 시 프로필 대신 로그인 버튼 보여줌
    
    * refactor: 시맨틱 태그로 변경, 콘솔로그 삭제, 버튼 텍스트 변경

--- src/pages/Layout/SideNav.tsx
+++ src/pages/Layout/SideNav.tsx
@@ -1,8 +1,10 @@
+import LoginButton from '@/components/LoginButton/LoginButton';
 import SideBarMyProfile from '@/components/SideBarMyProfile/SideBarMyProfile';
 import Avatar from '@/components/common/Avatar/Avatar';
 import { PATH } from '@/constants/path';
 import useClubs from '@/hooks/query/club/useClubs';
 import useUserImageQuery from '@/hooks/query/user/useUserImageQuery';
+import { getStorage } from '@/utils/localStorage';
 
 import { FaPlusCircle } from 'react-icons/fa';
 import { IoMdHome, IoMdNotifications } from 'react-icons/io';
@@ -13,6 +15,7 @@ import { ClubWrapper, CreateClubButtonStyled, SidebarContainer, iconStyle } from
 const SideNav = () => {
   const { clubs } = useClubs();
   const { userImage } = useUserImageQuery();
+  const isLoginUser = Boolean(getStorage('token'));
 
   const navigate = useNavigate();
 
@@ -30,14 +33,22 @@ const SideNav = () => {
           </Link>
         ))}
       </ClubWrapper>
-      <CreateClubButtonStyled>
-        <FaPlusCircle size={'1rem'} onClick={() => navigate(`${PATH.CREATE}`)} />
-      </CreateClubButtonStyled>
+      {isLoginUser && (
+        <CreateClubButtonStyled>
+          <FaPlusCircle size={'1rem'} onClick={() => navigate(`${PATH.CREATE}`)} />
+        </CreateClubButtonStyled>
+      )}
       <IoMdHome className={iconStyle} onClick={() => navigate('/')} />
       <IoMdNotifications className={iconStyle} onClick={() => alert('알림페이지 준비 중')} />
-      <Link to={`/profile`}>
-        <SideBarMyProfile profileImageUrl={userImage?.profileImageUrl} />
-      </Link>
+      {isLoginUser ? (
+        <Link to={`/profile`}>
+          <SideBarMyProfile profileImageUrl={userImage?.profileImageUrl} />
+        </Link>
+      ) : (
+        <Link to={PATH.LOGIN} style={{ textDecoration: 'none' }}>
+          <LoginButton />
+        </Link>
+      )}
     </SidebarContainer>
   );
 };
