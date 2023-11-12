7e5c77dbe86e77d967c75484d89fc5ecf9db8125
hyesung99 <4538asd@gmail.com>
2023-11-13 01:29:18 +0900

    feat : 페이지네이션 적용

--- src/pages/ProfilePage/AppliedEvents.tsx
+++ src/pages/ProfilePage/AppliedEvents.tsx
@@ -1,28 +1,44 @@
 import AppliedEventCard from '@/components/common/AppliedEventCard/AppliedEventCard';
+import Pagination from '@/components/common/Pagination/Pagination';
 import useAppliedEventQuery from '@/hooks/query/event/useAppliedEventQuery';
 
-import { AppliedEventContainer } from './ProfilePage.style';
+import { useState } from 'react';
+
+import { AppliedEventContainer, AppliedEventPaginationWrapper } from './ProfilePage.style';
 
 const AppliedEvents = () => {
-  const { events } = useAppliedEventQuery({ pageNumber: 0 });
+  const [currentPage, setCurrentPage] = useState(0);
 
-  if (!events) return null;
+  const { events, pageData } = useAppliedEventQuery({ pageNumber: currentPage });
+
+  if (!events || !pageData) return null;
+
+  const { totalPages, size } = pageData;
+
+  const onChangePage = (page: number) => {
+    setCurrentPage(page);
+  };
 
   return (
-    <AppliedEventContainer>
-      {events?.map((event) => (
-        <AppliedEventCard
-          key={event.id}
-          eventId={event.id}
-          title={event.title}
-          posterImageUrl={event.posterImageUrl}
-          startDate={event.startDate}
-          location={event.location}
-          clubName={event.clubName}
-          eventStatus={event.status}
-        />
-      ))}
-    </AppliedEventContainer>
+    <>
+      <AppliedEventContainer>
+        {events?.map((event) => (
+          <AppliedEventCard
+            key={event.id}
+            eventId={event.id}
+            title={event.title}
+            posterImageUrl={event.posterImageUrl}
+            startDate={event.startDate}
+            location={event.location}
+            clubName={event.clubName}
+            eventStatus={event.status}
+          />
+        ))}
+      </AppliedEventContainer>
+      <AppliedEventPaginationWrapper>
+        <Pagination totalPages={totalPages} size={size} onChangePage={onChangePage} />
+      </AppliedEventPaginationWrapper>
+    </>
   );
 };
 
