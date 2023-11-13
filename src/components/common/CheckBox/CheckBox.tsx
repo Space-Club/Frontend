ec7ff911739b57fea2ef1dc91fb49b9debe64ef6
hyesung99 <4538asd@gmail.com>
2023-11-13 17:22:50 +0900

    feat : CheckBox공통 컴포넌트 제작

---
+++ src/components/common/CheckBox/CheckBox.tsx
@@ -0,0 +1,52 @@
+import AlertModal from '@/components/Modals/AlertModal';
+import useModal from '@/hooks/useModal';
+import Theme from '@/styles/Theme';
+
+import { useState } from 'react';
+import { AiFillQuestionCircle } from 'react-icons/ai';
+
+import {
+  CheckBoxContainer,
+  CheckBoxInfoWrapper,
+  CheckBoxInputStyled,
+  CheckBoxLabelStyled,
+} from './CheckBox.style';
+
+interface CheckBoxProps {
+  onCheck: () => void;
+  onUncheck: () => void;
+  label: string;
+  info?: string;
+}
+
+const CheckBox = ({ onCheck, onUncheck, label, info }: CheckBoxProps) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const { modalClose, showModal, modalOpen } = useModal();
+
+  const handleCheckBoxChange = () => {
+    if (isChecked) {
+      onUncheck();
+    } else {
+      onCheck();
+    }
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <>
+      {showModal && <AlertModal message={info} onClose={modalClose} />}
+      <CheckBoxContainer>
+        {info && (
+          <CheckBoxInfoWrapper onClick={modalOpen}>
+            <AiFillQuestionCircle size={17} fill={Theme.color.activeColor} />
+          </CheckBoxInfoWrapper>
+        )}
+        <CheckBoxLabelStyled>{label}</CheckBoxLabelStyled>
+        <CheckBoxInputStyled type="checkbox" onChange={handleCheckBoxChange} />
+      </CheckBoxContainer>
+    </>
+  );
+};
+
+export default CheckBox;
