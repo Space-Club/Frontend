ee5adf58fac76cb111467b01a8e3b10cf42869cc
SongInJae <pan0663@naver.com>
2023-11-13 15:15:47 +0900

    SKRF-254 feat: event form UI 구현 (#76)
    * 홍보 폼 UI 완성
    
    * feat: 클럽 일정 UI 완성
    
    * feat: 피그마 수정에 따른 폼변경
    
    * fix: console 제거

--- src/pages/event/WriteEventInfoPage/WriteEventInfoPage.style.ts
+++ src/pages/event/WriteEventInfoPage/WriteEventInfoPage.style.ts
@@ -4,29 +4,35 @@ import styled from '@emotion/styled';
 
 const PerformanceFormContainer = styled.form`
   display: flex;
-  width: 100%;
-  padding: 3rem 0;
-`;
-const ContentArea = styled.div`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
   position: relative;
+  width: 100%;
+  height: 100%;
+  padding: 3rem 0;
 
-  &:first-child {
+  & > div:first-of-type {
     padding-right: 1.5rem;
     border-right: 1px solid ${Theme.color.tSeparator};
   }
-  &:last-child {
+
+  & > div:nth-last-of-type(2) {
     padding-left: 1.5rem;
   }
+  & > div:first-child > div:nth-last-of-type(2) {
+    flex-grow: 1;
+  }
+`;
+const ContentArea = styled.div`
+  width: 50%;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `;
 const TwoInputContainer = styled.div`
   display: flex;
   gap: 0.5rem;
 
-  &:last-child {
+  &:last-of-type {
     padding-top: 1rem;
     border-top: 1px solid ${Theme.color.tSeparator};
   }
@@ -34,14 +40,18 @@ const TwoInputContainer = styled.div`
 const HalfInputForm = styled(InputForm)`
   width: 50%;
 `;
-const SubmitButton = styled.button`
+const ButtonWrapper = styled.div`
+  display: flex;
   position: absolute;
-  bottom: 0;
   right: 0;
+  bottom: -2rem;
+  gap: 1rem;
+  padding: 1rem 0;
+`;
+const Button = styled.button`
   width: 6rem;
   height: 3rem;
   padding: 0.5rem;
-  background-color: ${Theme.color.tSemiActive};
   box-sizing: border-box;
   border: none;
   border-radius: 0.25rem;
@@ -49,7 +59,13 @@ const SubmitButton = styled.button`
   cursor: pointer;
   color: white;
 `;
-const ErrorMessage = styled.div`
+const PrevButton = styled(Button)`
+  background-color: ${Theme.color.idkGrey};
+`;
+const SubmitButton = styled(Button)`
+  background-color: ${Theme.color.tSemiActive};
+`;
+const ErrorMessage = styled.p`
   font-size: 0.7rem;
   color: red;
 `;
@@ -59,6 +75,8 @@ export {
   ContentArea,
   TwoInputContainer,
   HalfInputForm,
+  ButtonWrapper,
+  PrevButton,
   SubmitButton,
   ErrorMessage,
 };
