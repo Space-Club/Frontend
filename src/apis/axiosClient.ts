5de56a31510b4e59479b96c3f210359bf3b744ff
SongInJae <pan0663@naver.com>
2023-11-14 13:31:25 +0900

    -feat: 폼 작성 후, 신청자 적는 폼으로 이동하도록 기능 구현

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -8,7 +8,7 @@ export const axiosClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  baseURL: 'https://spaceclub.site/api/v1',
+  //baseURL: 'https://spaceclub.site/api/v1',
   withCredentials: true,
   timeout: NETWORK_TIMEOUT,
 });
