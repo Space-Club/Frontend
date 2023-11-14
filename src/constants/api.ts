f33e0a7dbac490ceb912d199860561b73300d358
hyesung99 <4538asd@gmail.com>
2023-11-14 16:19:00 +0900

    rename : 행사 신청 취소 엔드포인트명 변경

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -14,7 +14,7 @@ const END_POINTS = {
   GET_EVENT_DETAIL: '/events',
   ALL_EVENTS: '/events',
   POST_EVENT_APPLY: '/events/apply',
-  CANCEL_EVENT: ({ eventId }: { eventId: string }) => `/events/${eventId}/cancel`,
+  CANCEL_EVENT: ({ eventId }: { eventId: string }) => `/events/${eventId}/applications`,
 
   CREATE_CLUB: '/clubs',
   INVITE_LINK: (clubId: string) => `/clubs/${clubId}/invite`,
