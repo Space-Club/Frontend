767683685bdd344469e056b1e5aa6173956e4feb
SongInJae <pan0663@naver.com>
2023-11-14 17:16:55 +0900

    feat: 폼 신청 api 구현

--- src/apis/event/postApplyEvent.ts
+++ src/apis/event/postApplyEvent.ts
@@ -1,4 +1,5 @@
 import { END_POINTS } from '@/constants/api';
+<<<<<<< HEAD
 import { postEventApplyRequest } from '@/types/forms';
 
 import { axiosClientWithAuth } from '../axiosClient';
@@ -8,6 +9,13 @@ const postApplyEvent = async ({ forms, eventId }: postEventApplyRequest) => {
     eventId,
     forms,
   };
+=======
+import { postEventApplyRequest } from '@/types/event';
+
+import { axiosClientWithAuth } from '../axiosClient';
+
+const postApplyEvent = async (data: postEventApplyRequest) => {
+>>>>>>> 560e010 (feat: 폼 신청 api 구현)
   await axiosClientWithAuth.post(END_POINTS.EVENT_APPLY, data);
 };
 
