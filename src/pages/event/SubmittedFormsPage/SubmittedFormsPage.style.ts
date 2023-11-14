ebfda6424c6eb8f6e03aac1d21b39b6faef2053b
colorkite10 <colorkite10@gmail.com>
2023-11-15 02:50:50 +0900

    design: 제출된 폼 페이지 디자인 ing

---
+++ src/pages/event/SubmittedFormsPage/SubmittedFormsPage.style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const SubmittedFormsPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export { SubmittedFormsPageContainer };
