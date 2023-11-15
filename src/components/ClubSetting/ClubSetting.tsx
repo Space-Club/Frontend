9ed94af2948e4a1b426d3c45bdae7c5fcdf36181
hyesung99 <4538asd@gmail.com>
2023-11-15 18:38:33 +0900

    feat : 클럽 이름, 소개 수정 기능 추가

--- src/components/ClubSetting/ClubSetting.tsx
+++ src/components/ClubSetting/ClubSetting.tsx
@@ -1,9 +1,13 @@
 import useEditClubMutation from '@/hooks/query/club/useEditClubMutation';
 import useGetClubQuery from '@/hooks/query/club/useGetClubQuery';
+import { PurpleButton } from '@/pages/event/EventDetailPage/EventDetailPage.style';
+
+import { useRef } from 'react';
 
 import Avatar from '../common/Avatar/Avatar';
 import ClubBanner from '../common/ClubBanner/ClubBanner';
 import ClubCover from '../common/ClubCover/ClubCover';
+import InputForm from '../common/InputForm/InputForm';
 
 interface ClubSettingProps {
   clubId: string;
@@ -13,6 +17,9 @@ const ClubSetting = ({ clubId }: ClubSettingProps) => {
   const { clubInfo } = useGetClubQuery({ clubId });
   const { editClub } = useEditClubMutation();
 
+  const clubNameInputRef = useRef<HTMLInputElement>(null);
+  const clubInfoInputRef = useRef<HTMLInputElement>(null);
+
   const handleCoverImageEdit = (file: File) => {
     editClub({ image: file, clubId });
   };
@@ -20,6 +27,18 @@ const ClubSetting = ({ clubId }: ClubSettingProps) => {
   // const handleAvatarImageEdit = (file: File) => {
   //   editClub({ logoImage: file, clubId });
   // };
+
+  const handleClubNameEdit = () => {
+    if (clubNameInputRef.current) {
+      editClub({ name: clubNameInputRef.current.value, clubId });
+    }
+  };
+
+  const handleClubInfoEdit = () => {
+    if (clubInfoInputRef.current) {
+      editClub({ info: clubInfoInputRef.current.value, clubId });
+    }
+  };
 
   if (!clubInfo) return null;
 
@@ -32,6 +51,10 @@ const ClubSetting = ({ clubId }: ClubSettingProps) => {
         isEditable
         // onEdit={handleImageEdit}
       />
+      <InputForm ref={clubNameInputRef} labelText="클럽이름" inputType="text" />
+      <PurpleButton onClick={handleClubNameEdit}>수정</PurpleButton>
+      <InputForm ref={clubInfoInputRef} labelText="클럽소개" inputType="text" />
+      <PurpleButton onClick={handleClubInfoEdit}>수정</PurpleButton>
       <ClubCover
         coverImageUrl={clubInfo.logoImageUrl}
         clubId={clubId}
