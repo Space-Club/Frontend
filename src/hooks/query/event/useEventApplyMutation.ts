45483ebcb2366944e1be386d2d34f4eb03e387f5
SongInJae <pan0663@naver.com>
2023-11-15 17:57:39 +0900

    fix: 충돌 해결3

--- src/hooks/query/event/useEventApplyMutation.ts
+++ src/hooks/query/event/useEventApplyMutation.ts
@@ -1,15 +1,11 @@
 import postApplyEvent from '@/apis/event/postApplyEvent';
-<<<<<<< HEAD
 import useToast from '@/hooks/useToast';
 
 import { useNavigate } from 'react-router-dom';
-=======
->>>>>>> 14e8093 (feat: 폼 제출 api 모킹 및 api 최종 구현 완료)
 
 import { useMutation } from '@tanstack/react-query';
 
 const useEventApplyMutation = () => {
-<<<<<<< HEAD
   const navigate = useNavigate();
   const { createToast } = useToast();
 
@@ -19,10 +15,6 @@ const useEventApplyMutation = () => {
       createToast({ message: '성공적으로 신청되었습니다.', toastType: 'success' });
       navigate(-1);
     },
-=======
-  const { mutate: applyEvent } = useMutation({
-    mutationFn: postApplyEvent,
->>>>>>> 14e8093 (feat: 폼 제출 api 모킹 및 api 최종 구현 완료)
   });
 
   return { applyEvent, isApplyLoading };
