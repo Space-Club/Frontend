c400573b1b8c856626b5f7c727447fbb0d10053b
hyesung99 <4538asd@gmail.com>
2023-11-16 11:46:50 +0900

    feat : BookedEvents 컴포넌트 추가

--- src/pages/ProfilePage/BookedEvents.tsx
+++ src/pages/ProfilePage/BookedEvents.tsx
@@ -1,6 +1,45 @@
+import BookmarkedEventCard from '@/components/BookmarkedEventCard/BookmarkedEventCard';
+import Pagination from '@/components/common/Pagination/Pagination';
+import useGetEventBookMarkQuery from '@/hooks/query/event/useGetEventBookMarkQuery';
+
+import { useState } from 'react';
+
 const BookedEvents = () => {
-  //#TODO: 북마크한 행사 불러오는 api
-  return <div>북마크한 행사</div>;
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const { bookmarks, pageData } = useGetEventBookMarkQuery({
+    page: currentPage,
+    size: 10,
+    sort: 'id',
+  });
+
+  if (!bookmarks || !pageData) return null;
+
+  const { totalPages, size } = pageData;
+
+  const handleChangePage = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  return (
+    <>
+      {bookmarks.map((bookmark) => {
+        return (
+          <BookmarkedEventCard
+            key={bookmark.id}
+            id={bookmark.id}
+            title={bookmark.title}
+            location={bookmark.location}
+            clubName={bookmark.clubName}
+            posterImageUrl={bookmark.posterImageUrl}
+            startDate={bookmark.startDate}
+            bookmark={bookmark.bookmark}
+          />
+        );
+      })}
+      <Pagination totalPages={totalPages} size={size} onChangePage={handleChangePage} />
+    </>
+  );
 };
 
 export default BookedEvents;
