2eb15994c7757b8297d03f64111b433cb6020323
colorkite10 <colorkite10@gmail.com>
2023-11-17 23:47:44 +0900

    feat: 제출된 폼 페이지 페이지네이션 추가

--- src/pages/event/SubmittedFormsPage/SubmittedFormsPage.tsx
+++ src/pages/event/SubmittedFormsPage/SubmittedFormsPage.tsx
@@ -1,15 +1,36 @@
 import SearchInputForm from '@/components/SearchInputForm/SearchInputForm';
 import SubmittedForms from '@/components/SubmittedForms/SubmittedForms';
 import Header from '@/components/common/Header/Header';
+import Pagination from '@/components/common/Pagination/Pagination';
+import useGetSubmittedFormsQuery from '@/hooks/query/event/useGetSubmittedFormsQuery';
+
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const SubmittedFormsPage = () => {
-  //#TODO: 머지된 뒤 페이지 만들기
+  const { eventId } = useParams() as { eventId: string };
+  const [currentPage, setCurrentPage] = useState(0);
+  const { formInfo, userForms, pageData } = useGetSubmittedFormsQuery({
+    eventId,
+    pageNumber: currentPage,
+  });
+  if (!formInfo || !userForms || !pageData) {
+    return null;
+  }
+
+  const { totalPages, size } = pageData;
+
+  const handleChangePage = (page: number) => {
+    setCurrentPage(page);
+  };
+
   return (
     <>
       <Header>
         <SearchInputForm />
       </Header>
-      <SubmittedForms />
+      <SubmittedForms formInfo={formInfo} userForms={userForms} />
+      <Pagination totalPages={totalPages} size={size} onChangePage={handleChangePage} />
     </>
   );
 };
