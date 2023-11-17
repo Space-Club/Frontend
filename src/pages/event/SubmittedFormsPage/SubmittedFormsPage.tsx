d662ba055dceaf7d7f5ca73b21d74c4cef94c298
colorkite10 <colorkite10@gmail.com>
2023-11-18 00:23:30 +0900

    design: 제출된 폼 확인 페이지 레이아웃 구현

--- src/pages/event/SubmittedFormsPage/SubmittedFormsPage.tsx
+++ src/pages/event/SubmittedFormsPage/SubmittedFormsPage.tsx
@@ -2,10 +2,18 @@ import SearchInputForm from '@/components/SearchInputForm/SearchInputForm';
 import SubmittedForms from '@/components/SubmittedForms/SubmittedForms';
 import Header from '@/components/common/Header/Header';
 import Pagination from '@/components/common/Pagination/Pagination';
+import Tab from '@/components/common/Tab/Tab';
+import { MAIN_TABS } from '@/constants/tab';
 import useGetSubmittedFormsQuery from '@/hooks/query/event/useGetSubmittedFormsQuery';
 
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+import {
+  HeaderContentWrapper,
+  PaginationWrapper,
+  SubmittedFormsWrapper,
+} from './SubmittedFormsPage.style';
 
 const SubmittedFormsPage = () => {
   const { eventId } = useParams() as { eventId: string };
@@ -27,10 +35,17 @@ const SubmittedFormsPage = () => {
   return (
     <>
       <Header>
-        <SearchInputForm />
+        <HeaderContentWrapper>
+          <SearchInputForm />
+          <Tab tabItems={MAIN_TABS} />
+        </HeaderContentWrapper>
       </Header>
-      <SubmittedForms formInfo={formInfo} userForms={userForms} />
-      <Pagination totalPages={totalPages} size={size} onChangePage={handleChangePage} />
+      <SubmittedFormsWrapper>
+        <SubmittedForms formInfo={formInfo} userForms={userForms} />
+      </SubmittedFormsWrapper>
+      <PaginationWrapper>
+        <Pagination totalPages={totalPages} size={size} onChangePage={handleChangePage} />
+      </PaginationWrapper>
     </>
   );
 };
