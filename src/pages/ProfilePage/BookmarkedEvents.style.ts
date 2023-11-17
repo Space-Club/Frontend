224826d671381e24bf497b8bb1169bbca536cf71
hyesung99 <4538asd@gmail.com>
2023-11-17 17:07:30 +0900

    refactor : flex => grid로 변경

--- src/pages/ProfilePage/BookmarkedEvents.style.ts
+++ src/pages/ProfilePage/BookmarkedEvents.style.ts
@@ -1,12 +1,21 @@
 import styled from '@emotion/styled';
 
 const BookmarkedEventsContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  margin: 4rem 0;
-  gap: 1rem;
   width: 100%;
+  margin-top: 4rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 2rem;
+  justify-items: center;
+  align-items: center;
+
+  @media (max-width: 1400px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (max-width: 1040px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export { BookmarkedEventsContainer };
