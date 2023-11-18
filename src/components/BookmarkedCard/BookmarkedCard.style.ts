92274d763b5f4408bb1f0142412e07fa257272c6
hyesung99 <4538asd@gmail.com>
2023-11-18 14:15:23 +0900

    refactor : 마지막 줄에는 세퍼레이터 삭제

--- src/components/BookmarkedCard/BookmarkedCard.style.ts
+++ src/components/BookmarkedCard/BookmarkedCard.style.ts
@@ -6,16 +6,6 @@ const BookmarkedCardContainer = styled.div`
   display: flex;
   width: 20rem;
   position: relative;
-  //세퍼레이터 추가
-  ::after {
-    content: '';
-    position: absolute;
-    width: 90%;
-    height: 100%;
-    border-bottom: 1px solid ${Theme.color.tSeparator};
-    top: 1rem;
-    right: 5%;
-  }
 `;
 
 const BookmarkedTitleStyled = styled.h3`
--- src/pages/ProfilePage/BookmarkedEvents.style.ts
+++ src/pages/ProfilePage/BookmarkedEvents.style.ts
@@ -1,3 +1,4 @@
+import Theme from '@/styles/Theme';
 import styled from '@emotion/styled';
 
 const BookmarkedEventsContainer = styled.div`
@@ -5,16 +6,44 @@ const BookmarkedEventsContainer = styled.div`
   margin-top: 4rem;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 2rem;
+  grid-gap: 4rem 3rem;
   justify-items: center;
   align-items: center;
 
+  & > div:not(:nth-of-type(-n + 3)):after {
+    content: '';
+    position: absolute;
+    width: 90%;
+    height: 100%;
+    border-top: 1px solid ${Theme.color.tSeparator};
+    top: -2rem;
+    right: 5%;
+  }
+
   @media (max-width: 1400px) {
     grid-template-columns: 1fr 1fr;
+    & > div:not(:nth-of-type(-n + 2)):after {
+      content: '';
+      position: absolute;
+      width: 90%;
+      height: 100%;
+      border-top: 1px solid ${Theme.color.tSeparator};
+      top: -1rem;
+      right: 5%;
+    }
   }
 
   @media (max-width: 1040px) {
     grid-template-columns: 1fr;
+    & > div:not(:nth-of-type(-n + 1)):after {
+      content: '';
+      position: absolute;
+      width: 90%;
+      height: 100%;
+      border-top: 1px solid ${Theme.color.tSeparator};
+      top: -1rem;
+      right: 5%;
+    }
   }
 `;
 
