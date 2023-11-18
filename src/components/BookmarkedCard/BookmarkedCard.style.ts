e7275ea19fe3157f2ad87ccf9d6812541b08c0dc
hyesung99 <4538asd@gmail.com>
2023-11-18 22:05:11 +0900

    feat : white-space no-wrap처리

--- src/components/BookmarkedCard/BookmarkedCard.style.ts
+++ src/components/BookmarkedCard/BookmarkedCard.style.ts
@@ -11,24 +11,23 @@ const BookmarkedCardContainer = styled.div`
 const BookmarkedTitleStyled = styled.h3`
   font-size: 1.375rem;
   font-weight: 400;
-  margin-bottom: 1.2rem;
+  margin-bottom: 1.8rem;
 `;
 
 const BookmarkedInfoStyled = styled(ellipsisText)`
   color: ${Theme.color.semiBlack};
-  font-size: 1rem;
   font-weight: 400;
 `;
 
 const BookmarkedInfoLine = styled.div`
   display: flex;
-  margin-bottom: 0.1rem;
+  margin-bottom: 0.5rem;
 `;
 
 const BookmarkedInfoWrapper = styled.div`
   width: 12rem;
+  font-size: 0.9rem;
   overflow: hidden;
-
   padding: 1rem;
   display: flex;
   flex-direction: column;
@@ -36,7 +35,8 @@ const BookmarkedInfoWrapper = styled.div`
 
 const BookmarkedInfoLabelStyled = styled.p`
   color: ${Theme.color.black};
-  font-size: 1rem;
+  white-space: nowrap;
+  margin-right: 0.3rem;
 `;
 
 export {
