36eb9a7dc66ee549298add5d09d50bb83bd01db8
hyesung99 <4538asd@gmail.com>
2023-11-20 18:29:45 +0900

    feat : 유저 탈퇴 훅 추가

---
+++ src/hooks/query/user/useDeleteUserMutation.ts
@@ -0,0 +1,24 @@
+import deleteUser from '@/apis/users/deleteUser';
+import { PATH } from '@/constants/path';
+import useToast from '@/hooks/useToast';
+
+import { useNavigate } from 'react-router-dom';
+
+import { useMutation } from '@tanstack/react-query';
+
+const useDeleteUserMutation = () => {
+  const navigate = useNavigate();
+  const { createToast } = useToast();
+
+  const { mutate } = useMutation({
+    mutationFn: deleteUser,
+    onSuccess: () => {
+      createToast({ message: '회원탈퇴가 완료되었습니다.', toastType: 'success' });
+      navigate(PATH.LOGIN);
+    },
+  });
+
+  return { mutate };
+};
+
+export default useDeleteUserMutation;
