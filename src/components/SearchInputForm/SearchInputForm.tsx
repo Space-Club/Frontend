e79f1b5e2f8ef1f0b5f7a6ad3f52630e9f0e53e2
colorkite10 <colorkite10@gmail.com>
2023-11-20 18:22:26 +0900

    feat: useDebounce 훅 구현

--- src/components/SearchInputForm/SearchInputForm.tsx
+++ src/components/SearchInputForm/SearchInputForm.tsx
@@ -1,5 +1,7 @@
 import { PATH } from '@/constants/path';
+import useDebounceValue from '@/hooks/useDebounce';
 
+import { useState } from 'react';
 import { CiSearch } from 'react-icons/ci';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,12 +13,20 @@ import {
 } from './SearchInputForm.style';
 
 const SearchInputForm = () => {
+  const [keyword, setKeyword] = useState('');
+  const debouncedKeyword = useDebounceValue(keyword, 1000);
   const navigate = useNavigate();
+
+  console.log(debouncedKeyword);
 
   return (
     <SearchInputContainerStyled>
       <SearchBarStyled>
-        <SearchInputStyled placeholder="검색하기" />
+        <SearchInputStyled
+          placeholder="검색하기"
+          value={keyword}
+          onChange={(event) => setKeyword(event.target.value)}
+        />
         <IconContainerStyled onClick={() => navigate(PATH.SEARCH)}>
           <CiSearch size="1.5rem" />
         </IconContainerStyled>
