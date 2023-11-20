ace56b6912db78d7009358ff32a7e41d4c7a4b40
SongInJae <pan0663@naver.com>
2023-11-20 17:52:03 +0900

    feat: 이벤트 삭제 기능 구현 (#116)

--- src/apis/event/deleteEvent.ts
+++ src/apis/event/deleteEvent.ts
@@ -1,13 +1,13 @@
 import { END_POINTS } from '@/constants/api';
 
-import { axiosClient } from '../axiosClient';
+import { axiosClientWithAuth } from '../axiosClient';
 
 interface deleteEvent {
   eventId: string;
 }
 
 const deleteEvent = async ({ eventId }: deleteEvent) => {
-  await axiosClient.delete(`${END_POINTS.GET_EVENT_DETAIL}/${eventId}`);
+  await axiosClientWithAuth.delete(`${END_POINTS.GET_EVENT_DETAIL}/${eventId}`);
 };
 
 export default deleteEvent;
