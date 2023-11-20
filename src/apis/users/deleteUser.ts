f6b36325822d8a2055400d1df5e47d965afb8cbe
hyesung99 <4538asd@gmail.com>
2023-11-20 23:44:26 +0900

    fix : api header contentType 수정

--- src/apis/users/deleteUser.ts
+++ src/apis/users/deleteUser.ts
@@ -3,7 +3,11 @@ import { END_POINTS } from '@/constants/api';
 import { axiosClientWithAuth } from '../axiosClient';
 
 const deleteUser = async () => {
-  await axiosClientWithAuth.delete(END_POINTS.DELETE_USER);
+  await axiosClientWithAuth.delete(END_POINTS.DELETE_USER, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
 };
 
 export default deleteUser;
