83807e7007fb30f791abcc04bdf11b5d68f7cbfa
hyesung99 <4538asd@gmail.com>
2023-11-20 17:31:30 +0900

    feat : 프로필 이미지 수정 시 사이드 바도 invalid

--- src/hooks/query/user/usePatchUserImageMutation.ts
+++ src/hooks/query/user/usePatchUserImageMutation.ts
@@ -2,14 +2,16 @@ import patchUserImage from '@/apis/users/patchUserImage';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { QUERY_KEY } from './useMyProfile';
+import { QUERY_KEY as PROFILE_KEY } from './useMyProfile';
+import { QUERY_KEY as SIDE_BAR_KEY } from './useUserImageQuery';
 
 const usePatchUserImageMutation = () => {
   const queryClient = useQueryClient();
   const { mutate: editUserImage } = useMutation({
     mutationFn: patchUserImage,
     onSuccess: () => {
-      queryClient.invalidateQueries([QUERY_KEY.MY_PROFILE]);
+      queryClient.invalidateQueries([PROFILE_KEY.MY_PROFILE]);
+      queryClient.invalidateQueries([SIDE_BAR_KEY.USER_IMAGE]);
     },
   });
   return { editUserImage };
