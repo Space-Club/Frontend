a9c53803a819aaf3343b03b94f62c00741697e09
hyesung99 <4538asd@gmail.com>
2023-11-20 17:25:05 +0900

    feat : 수정하기 버튼 누르면 테두리 생김

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,21 @@
 import { ERROR_MESSAGE } from '@/constants/errorMessage';
 import useMyProfile from '@/hooks/query/user/useMyProfile';
 import usePatchUserImageMutation from '@/hooks/query/user/usePatchUserImageMutation';
+import { PurpleButton } from '@/pages/event/EventDetailPage/EventDetailPage.style';
 import { validateName, validateNumber } from '@/utils/validate';
 
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import Avatar from '../common/Avatar/Avatar';
 import InputForm from '../common/InputForm/InputForm';
-import { EditButton, InfoWrapper, ProfileContainer } from './Profile.style';
+import { InfoWrapper, ProfileButtonsWrapper, ProfileContainer } from './Profile.style';
 
 const Profile = () => {
+  const [isEdit, setIsEdit] = useState(false);
   const { register, setValue } = useForm();
   const { editUserImage } = usePatchUserImageMutation();
+
   const { data } = useMyProfile({ setValue });
 
   const { REQUIRED_NAME, REQUIRED_NUMBER, NAME } = ERROR_MESSAGE.REGISTER;
@@ -25,7 +29,13 @@ const Profile = () => {
         onEdit={(file) => editUserImage({ userImage: file })}
       />
       <InfoWrapper>
-        <EditButton buttonText="정보 수정하기" />
+        <ProfileButtonsWrapper>
+          {!isEdit ? (
+            <PurpleButton onClick={() => setIsEdit(true)}>수정하기</PurpleButton>
+          ) : (
+            <PurpleButton onClick={() => setIsEdit(false)}>저장</PurpleButton>
+          )}
+        </ProfileButtonsWrapper>
         <InputForm
           {...register('username', {
             required: REQUIRED_NAME,
@@ -33,10 +43,12 @@ const Profile = () => {
             maxLength: { value: 10, message: `${NAME}` },
             validate: validateName,
           })}
+          editable={isEdit}
           inputType="text"
         />
         <InputForm
           {...register('phoneNumber', { required: REQUIRED_NUMBER, validate: validateNumber })}
+          editable={isEdit}
           inputType="text"
         />
       </InfoWrapper>
