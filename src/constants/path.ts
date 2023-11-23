5523a0216f7251d95054a2a472acbc8d8170ad02
hyesung99 <4538asd@gmail.com>
2023-11-23 16:31:00 +0900

    refactor : PATH 상수화

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -3,14 +3,15 @@ import { eventTypeAPI } from '@/types/event';
 const PATH = {
   LOGIN: '/login',
   REGISTER: '/register',
-  OAUTH_REDIRECT: '/oauth/kakao',
+  OAUTH_REDIRECT: '/oauth/kakao/callback',
 
   MAIN: '/',
   MAIN_EVENT: '/events',
   MAIN_RECRUITMENT: '/recruitment',
+  PROFILE: '/profile:category',
   PROFILE_APPLIED: '/profile/applied',
   PROFILE_BOOKMARK: '/profile/bookmark',
-  SEARCH: '/search',
+  SEARCH: (keyword: string) => `/search/${keyword}`,
 
   CLUB: {
     HOME: (clubId: string | number) => `/club/${clubId}/home`,
@@ -20,13 +21,15 @@ const PATH = {
     WRITE_INFO: (clubId: string) => `/club/${clubId}/writeinfo`,
     WRITE_FORM: (clubId: string | number, eventId: string | number, eventType: eventTypeAPI) =>
       `/club/${clubId}/writeform/${eventId}?event=${eventType}`,
+    INVITE: (inviteCode: string) => `clubs/invite/${inviteCode}`,
   },
   CREATE: '/create',
 
   EVENT: {
     DETAIL: (eventId: string) => `/event/${eventId}`,
-    CHECK_FORM: '/event/checkform',
-    SUBMIT_FORM: '/event/submitform',
+    SUBMITTED_FORMS: (eventId: string) => `/event/${eventId}/forms`,
+    SUBMIT_FORM: (eventId: string) => `/event/${eventId}/submitform`,
+
     WRITE_FORM: '/event/writeform',
   },
 } as const;
