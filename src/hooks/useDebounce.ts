61add6c68c7eedd9397867cc393496908cda05d4
colorkite10 <colorkite10@gmail.com>
2023-11-23 16:10:58 +0900

    refactor: 디바운스 시간 1000->300, 엔터 누르면 해당 keyword 바로 검색

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-export default function useDebounceValue<T>(value: T, delay: number = 1000) {
+export default function useDebounceValue<T>(value: T, delay: number = 300) {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
   useEffect(() => {
     const handler = setTimeout(() => {
