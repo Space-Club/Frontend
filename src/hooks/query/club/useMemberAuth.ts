0a80779609ca9c49884f272cb8d77e2329ac27d8
hyesung99 <4538asd@gmail.com>
2023-11-23 16:32:08 +0900

    feat : useMemberAuth retry 0 설정

--- src/hooks/query/club/useMemberAuth.ts
+++ src/hooks/query/club/useMemberAuth.ts
@@ -1,19 +1,20 @@
 import getMemberAuth from '@/apis/club/getMemberAuth';
-import { MemberAuthRequest } from '@/types/api/memberAuth';
+import { MemberAuthRequest, MemberAuthResponse } from '@/types/api/memberAuth';
 
 import { useQuery } from '@tanstack/react-query';
+
+import { AxiosError } from 'axios';
 
 export const QUERY_KEY = { MEMBER_AUTH: 'MEMBER_AUTH' };
 
 const useMemberAuth = ({ clubId }: MemberAuthRequest) => {
-  const { data } = useQuery({
+  const { data, error, isError } = useQuery<MemberAuthResponse, AxiosError>({
     queryKey: [QUERY_KEY.MEMBER_AUTH, clubId],
     queryFn: () => getMemberAuth({ clubId }),
+    retry: 0,
   });
 
-  const { role } = data ?? {};
-
-  return { role };
+  return { role: data?.role, errorCode: error?.response?.data, isError };
 };
 
 export default useMemberAuth;
