f2328bd2074a962b83a8f683f6edab9cfb0db770
hyesung99 <4538asd@gmail.com>
2023-11-26 20:07:36 +0900

    refactor : 관리모드 설명 수정

--- src/constants/modalMessage.ts
+++ src/constants/modalMessage.ts
@@ -5,8 +5,7 @@ const MODAL_TEXT = {
   TICKET_APPLY: '예매 장수를 선택해주세요.',
   EVENT_APPLY: '행사를 신청하시겠습니까?',
   DELETE_MEMBER: '정말 이 멤버를 탈퇴시키시겠어요?',
-  MANAGE_INFO: `관리 모드를 사용하면 제출한 폼에 대해 관리를 할 수 있습니다! 
-  참여승인을 위해서 관리자의 확인이 필요한 경우 유용하게 쓸 수 있어요.`,
+  MANAGE_INFO: `관리 모드를 사용하면 제출한 폼에 대해 관리를 할 수 있습니다!`,
   FORM_OPTION_SKIP_CONFIRM:
     '신청자의 정보를 받지 않으면 참여자의 정보를 볼 수 없어요! 그래도 진행하시겠어요?',
   DELETE_EVENT: '정말 행사를 삭제하시겠습니까?',
