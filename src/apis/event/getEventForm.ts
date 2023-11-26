68dfd4ef3237270d7cf8901d27d6bc7968c09ca1
SongInJae <pan0663@naver.com>
2023-11-26 10:50:00 +0900

    fix: 오류 수정

--- src/apis/event/getEventForm.ts
+++ src/apis/event/getEventForm.ts
@@ -1,11 +1,10 @@
 import { END_POINTS } from '@/constants/api';
-import { getEventDetailResponse } from '@/types/api/getEventDetail';
-import { getEventFormRequest } from '@/types/api/getEventForm';
+import { getEventFormRequest, getEventFormResponse } from '@/types/api/getEventForm';
 
 import { axiosClientWithAuth } from '../axiosClient';
 
 const getEventForm = async ({ eventId }: getEventFormRequest) => {
-  const { data } = await axiosClientWithAuth.get<getEventDetailResponse>(
+  const { data } = await axiosClientWithAuth.get<getEventFormResponse>(
     END_POINTS.GET_EVENT_FORM(eventId),
   );
 
