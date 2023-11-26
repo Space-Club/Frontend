6689a51c45fc86d514cc6d5660929b9fad02eaa4
hyesung99 <4538asd@gmail.com>
2023-11-26 19:09:30 +0900

    feat : 마지막 관리자 추방 토스트 메시지

--- src/hooks/query/user/useDeleteMemberMutation.ts
+++ src/hooks/query/user/useDeleteMemberMutation.ts
@@ -1,7 +1,11 @@
 import deleteMember from '@/apis/users/deleteMember';
+import { EXCEPTION_CODE } from '@/constants/exceptionCode';
 import useToast from '@/hooks/useToast';
+import { HttpException } from '@/types/common';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { AxiosError } from 'axios';
 
 import { QUERY_KEY } from '../club/useClubMembersQuery';
 
@@ -14,7 +18,11 @@ const useDeleteMemberMutation = () => {
     onSuccess: () => {
       queryClient.invalidateQueries([QUERY_KEY.GET_CLUB_MEMBERS]);
     },
-    onError: () => {
+    onError: (error: AxiosError<HttpException>) => {
+      if (error.response?.data.code === EXCEPTION_CODE.CAN_NOT_WITHDRAW) {
+        createToast({ message: '최소 한명의 관리자가 필요합니다.', toastType: 'error' });
+        return;
+      }
       createToast({ message: '멤버를 삭제하는 데 실패했습니다. ', toastType: 'error' });
     },
   });
