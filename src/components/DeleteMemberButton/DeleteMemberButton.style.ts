b1257ae8ec04cca377f41d22da032e5a00c0a73e
hyesung99 <4538asd@gmail.com>
2023-11-28 15:45:37 +0900

    rename : 탈퇴 => 내보내기

--- src/components/DeleteMemberButton/DeleteMemberButton.style.ts
+++ src/components/DeleteMemberButton/DeleteMemberButton.style.ts
@@ -2,14 +2,22 @@ import Theme from '@/styles/Theme';
 import styled from '@emotion/styled';
 
 const DeleteMemberButtonStyled = styled.button`
-  width: 1.8125rem;
   height: 1.25rem;
+  padding: 0 0.2rem;
+  margin-left: 0.2rem;
+  white-space: nowrap;
   border-radius: 0.3125rem;
   font-size: ${Theme.fontSize.tagText};
   background-color: ${Theme.color.tButtonWhite};
   border: none;
   cursor: pointer;
   outline: none;
+  transition: all 0.1s ease-in-out;
+
+  :hover {
+    background-color: ${Theme.color.tRed};
+    color: ${Theme.color.tButtonWhite};
+  }
 `;
 
 export { DeleteMemberButtonStyled };
