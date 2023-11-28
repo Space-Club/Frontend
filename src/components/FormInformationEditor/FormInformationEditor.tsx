ef43235389894c31d228b800201bd7dabdeb6d8c
hyesung99 <4538asd@gmail.com>
2023-11-29 00:18:15 +0900

    feat : 폼 설명 최대길이 제한 1000자

--- src/components/FormInformationEditor/FormInformationEditor.tsx
+++ src/components/FormInformationEditor/FormInformationEditor.tsx
@@ -17,8 +17,8 @@ const FormInformationEditor = () => {
     <>
       <FormInformationEditorContainer
         placeholder="폼에 대한 설명을 작성해주세요!"
-        contentEditable
         onBlur={handleBlur}
+        maxLength={1000}
       ></FormInformationEditorContainer>
     </>
   );
