394ba78f82035fe79d729af0f33e6b9141f0c139
SongInJae <pan0663@naver.com>
2023-11-28 23:15:08 +0900

    fix: 게시물 delete invalidQuery 적용

--- src/hooks/query/event/useDeleteEventMutation.ts
+++ src/hooks/query/event/useDeleteEventMutation.ts
@@ -1,7 +1,11 @@
 import deleteEvent from '@/apis/event/deleteEvent';
 import useToast from '@/hooks/useToast';
 
-import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { QUERY_KEY as GET_ALL_EVENT_QUERY_KEY } from './useAllEventsQuery';
 
 interface useDeleteEventMutation {
   eventId: string;
@@ -9,10 +13,14 @@ interface useDeleteEventMutation {
 
 const useDeleteEventMutation = ({ eventId }: useDeleteEventMutation) => {
   const { createToast } = useToast();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
   const { mutate: deleteEventMutate } = useMutation({
     mutationKey: ['delete_event', `${eventId}`],
     mutationFn: () => deleteEvent({ eventId }),
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [GET_ALL_EVENT_QUERY_KEY] });
+      navigate('/');
       createToast({ message: '행사가 성공적으로 삭제되었습니다.', toastType: 'success' });
     },
     onError: () => {
