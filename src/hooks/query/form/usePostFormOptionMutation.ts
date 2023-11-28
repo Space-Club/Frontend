2f44682c29fbeb5d4273d0b1b6845ea4e8b5e465
colorkite10 <colorkite10@gmail.com>
2023-11-28 16:06:25 +0900

    refactor: 행사 생성 시 클럽 행사와 모든 행사 query 무효화

--- src/hooks/query/form/usePostFormOptionMutation.ts
+++ src/hooks/query/form/usePostFormOptionMutation.ts
@@ -8,7 +8,8 @@ import { useNavigate } from 'react-router-dom';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { QUERY_KEY } from '../club/useClubEventsQuery';
+import { QUERY_KEY as CLUB_EVENTS_QUERY_KEY } from '../club/useClubEventsQuery';
+import { QUERY_KEY as ALL_EVENTS_QUERY_KEY } from '../event/useAllEventsQuery';
 
 interface usePostFormOptionMutationProps {
   eventId: string;
@@ -22,8 +23,15 @@ const usePostFormOptionMutation = ({ eventId }: usePostFormOptionMutationProps) 
   const { mutate: postOption } = useMutation({
     mutationFn: postFormOption,
     onSuccess: () => {
+      Promise.all([
+        queryClient.invalidateQueries([CLUB_EVENTS_QUERY_KEY.CLUB_EVENTS]),
+        queryClient.invalidateQueries([
+          ALL_EVENTS_QUERY_KEY.SHOW,
+          ALL_EVENTS_QUERY_KEY.PROMOTION,
+          ALL_EVENTS_QUERY_KEY.RECRUITMENT,
+        ]),
+      ]);
       createToast({ message: SUCCESS_MESSAGE.FORM.POST_OPTION, toastType: 'success' });
-      queryClient.invalidateQueries([QUERY_KEY.CLUB_EVENTS]);
       navigate(PATH.EVENT.DETAIL(eventId));
     },
     onError: () => {
