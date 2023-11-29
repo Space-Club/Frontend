f22a6b2e5c65f59ea6216083d9c9a0aadbf1e16e
colorkite10 <colorkite10@gmail.com>
2023-11-29 22:17:11 +0900

    refactor: 브레드크럼 함수 수정

--- src/constants/breadcrumb.ts
+++ src/constants/breadcrumb.ts
@@ -1,81 +1,38 @@
 import { PATH } from './path';
 
-const EVENT_DETAIL_BREADCRUMB = (
-  category: string | undefined,
-  eventId: string,
-  title = '상세보기',
-) => {
-  if (category === 'SHOW') {
-    return [
-      { title: '공연', link: PATH.MAIN },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-    ];
-  } else if (category === 'PROMOTION') {
-    return [
-      { title: '행사', link: PATH.MAIN_EVENT },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-    ];
-  } else if (category === 'RECRUITMENT') {
-    return [
-      { title: '모집공고', link: PATH.MAIN_RECRUITMENT },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-    ];
-  } else {
-    return null;
-  }
+const EVENT_DETAIL_BREADCRUMB = (category = '전체', eventId: string, title = '상세보기') => {
+  const categoryTitle =
+    category === 'SHOW' ? '공연' : category === 'PROMOTION' ? '행사' : '모집공고';
+  const categoryPath =
+    category === 'SHOW'
+      ? PATH.MAIN
+      : category === 'PROMOTION'
+      ? PATH.MAIN_EVENT
+      : PATH.MAIN_RECRUITMENT;
+
+  return [
+    { title: categoryTitle, link: categoryPath },
+    { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
+  ];
 };
 
 const SUBMITTED_FORMS_BREADCRUMB = (
-  category: string,
+  category = '전체',
   eventId: string,
   clubId: string,
   title = '상세보기',
 ) => {
-  if (category === 'SHOW') {
-    return [
-      { title: '공연', link: PATH.MAIN },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-      { title: '제출된 폼 보기', link: PATH.EVENT.SUBMITTED_FORMS(clubId, eventId) },
-    ];
-  } else if (category === 'PROMOTION') {
-    return [
-      { title: '행사', link: PATH.MAIN_EVENT },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-      { title: '제출된 폼 보기', link: PATH.EVENT.SUBMITTED_FORMS(clubId, eventId) },
-    ];
-  } else if (category === 'RECRUITMENT') {
-    return [
-      { title: '모집공고', link: PATH.MAIN_RECRUITMENT },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-      { title: '제출된 폼 보기', link: PATH.EVENT.SUBMITTED_FORMS(clubId, eventId) },
-    ];
-  } else {
-    return null;
-  }
+  return [
+    ...EVENT_DETAIL_BREADCRUMB(category, eventId, title),
+    { title: '제출된 폼', link: PATH.EVENT.SUBMITTED_FORMS(clubId, eventId) },
+  ];
 };
 
-const WRITE_APPLY_FORM_BREADCRUMB = (category: string, eventId: string, title = '상세보기') => {
-  if (category === 'SHOW') {
-    return [
-      { title: '공연', link: PATH.MAIN },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-      { title: '참여신청 폼 작성하기', link: PATH.EVENT.SUBMIT_FORM(eventId) },
-    ];
-  } else if (category === 'PROMOTION') {
-    return [
-      { title: '행사', link: PATH.MAIN_EVENT },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-      { title: '참여신청 폼 작성하기', link: PATH.EVENT.SUBMIT_FORM(eventId) },
-    ];
-  } else if (category === 'RECRUITMENT') {
-    return [
-      { title: '모집공고', link: PATH.MAIN_RECRUITMENT },
-      { title: `${title}`, link: PATH.EVENT.DETAIL(eventId) },
-      { title: '참여신청 폼 작성하기', link: PATH.EVENT.SUBMIT_FORM(eventId) },
-    ];
-  } else {
-    return null;
-  }
+const WRITE_APPLY_FORM_BREADCRUMB = (category = '전체', eventId: string, title = '상세보기') => {
+  return [
+    ...EVENT_DETAIL_BREADCRUMB(category, eventId, title),
+    { title: '참여신청 폼', link: PATH.EVENT.SUBMIT_FORM(eventId) },
+  ];
 };
 
 export { EVENT_DETAIL_BREADCRUMB, SUBMITTED_FORMS_BREADCRUMB, WRITE_APPLY_FORM_BREADCRUMB };
