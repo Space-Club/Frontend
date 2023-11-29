c13db804900a7b66f6662ee619c9ca178cdc6330
hyesung99 <4538asd@gmail.com>
2023-11-29 23:44:59 +0900

    refactor : onBlur => onChange

--- src/components/FormOptions/FormOptions.tsx
+++ src/components/FormOptions/FormOptions.tsx
@@ -22,7 +22,7 @@ const FormOptions = () => {
           key={option.id}
           option={option}
           onClose={deleteOption}
-          onBlur={(event) => handleOptionTitleChange(option, event)}
+          onChange={(event) => handleOptionTitleChange(option, event)}
         />
       ))}
     </>
