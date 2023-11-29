98adc29019ca16f21159bdf1d6d84329ca2ab2f0
colorkite10 <colorkite10@gmail.com>
2023-11-29 17:36:50 +0900

    feat: 검색결과 페이지 suspense 적용

--- src/pages/SearchResultPage/SearchResultPage.tsx
+++ src/pages/SearchResultPage/SearchResultPage.tsx
@@ -1,9 +1,11 @@
 import SearchInputForm from '@/components/SearchInputForm/SearchInputForm';
 import SearchedEvents from '@/components/SearchedEvents';
 import Header from '@/components/common/Header/Header';
+import Spinner from '@/components/common/Spinner/Spinner';
 import Tab from '@/components/common/Tab/Tab';
 import { MAIN_TABS } from '@/constants/tab';
 
+import { Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { SearchMessageStyled, SearchesContainer } from './SearchResultPage.style';
@@ -18,9 +20,11 @@ const SearchResultPage = () => {
         <Tab tabItems={MAIN_TABS} />
       </Header>
       <SearchMessageStyled>{`"${keyword}" 검색 결과`}</SearchMessageStyled>
-      <SearchesContainer>
-        <SearchedEvents keyword={keyword} />
-      </SearchesContainer>
+      <Suspense fallback={<Spinner />}>
+        <SearchesContainer>
+          <SearchedEvents keyword={keyword} />
+        </SearchesContainer>
+      </Suspense>
     </>
   );
 };
