b67e79a3cb3321383488b59e8b9345e8ddd6c231
SongInJae <pan0663@naver.com>
2023-11-29 14:00:07 +0900

    fix: useIsBookmarkQuery query-key key, value 이름 변경

--- src/hooks/query/event/useIsBookmarkQuery.ts
+++ src/hooks/query/event/useIsBookmarkQuery.ts
@@ -3,7 +3,7 @@ import { GetIsBookmarkRequest } from '@/types/api/getIsBookmark';
 
 import { useQuery } from '@tanstack/react-query';
 
-export const QUERY_KEY = { GET_IS_EVENT: 'GET_IS_EVENT' };
+export const QUERY_KEY = { GET_IS_BOOKMARKED: 'GET_IS_BOOKMARKED' };
 
 const useIsBookmarkQuery = ({ eventId }: GetIsBookmarkRequest) => {
   const { data: isBookmarked } = useQuery({
