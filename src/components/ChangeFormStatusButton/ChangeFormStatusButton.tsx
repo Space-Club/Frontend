132e9ed277ee94777fd1686d9003d0e4ec4fbef5
colorkite10 <colorkite10@gmail.com>
2023-11-29 14:34:21 +0900

    refactor: 참여신청 폼 상태 변경 시 모달 띄움

--- src/components/ChangeFormStatusButton/ChangeFormStatusButton.tsx
+++ src/components/ChangeFormStatusButton/ChangeFormStatusButton.tsx
@@ -1,9 +1,11 @@
 import useSubmittedFormStatusMutation from '@/hooks/query/form/useSubmittedFormStatusMutation';
+import useModal from '@/hooks/useModal';
 import { EventStatus } from '@/types/event';
 
 import { FaCheckCircle } from 'react-icons/fa';
 import { FaRegCheckCircle } from 'react-icons/fa';
 
+import ConfirmModal from '../Modals/ConfirmModal';
 import { ButtonValueStyled, StatusButtonContainer } from './ChangeFormStatusButton.style';
 
 interface ChangeFormStatusButtonProps {
@@ -14,33 +16,43 @@ interface ChangeFormStatusButtonProps {
 
 const ChangeFormStatusButton = ({ eventId, formId, status }: ChangeFormStatusButtonProps) => {
   const { changeSubmittedFormStatus } = useSubmittedFormStatusMutation();
+  const { showModal, modalOpen, modalClose } = useModal();
 
   return (
-    <StatusButtonContainer
-      status={status}
-      onClick={() =>
-        changeSubmittedFormStatus({
-          eventId,
-          formUserId: formId,
-          participationStatus: status === 'CONFIRMED' ? 'PENDING' : 'CONFIRMED',
-        })
-      }
-      disabled={status === 'CANCELED'}
-    >
-      <ButtonValueStyled>
-        {status === 'CONFIRMED' ? (
-          <>
-            <FaCheckCircle size={12} />
-            <span>확인</span>
-          </>
-        ) : (
-          <>
-            <FaRegCheckCircle size={12} />
-            <span>대기</span>
-          </>
-        )}
-      </ButtonValueStyled>
-    </StatusButtonContainer>
+    <>
+      {showModal && (
+        <ConfirmModal
+          message={`상태를 ${status === 'CONFIRMED' ? '"대기"로' : '"확인"으로'} 변경하시겠습니까?`}
+          onConfirm={() =>
+            changeSubmittedFormStatus({
+              eventId,
+              formUserId: formId,
+              participationStatus: status === 'CONFIRMED' ? 'PENDING' : 'CONFIRMED',
+            })
+          }
+          onClose={modalClose}
+        />
+      )}
+      <StatusButtonContainer
+        status={status}
+        onClick={() => modalOpen()}
+        disabled={status === 'CANCELED'}
+      >
+        <ButtonValueStyled>
+          {status === 'CONFIRMED' ? (
+            <>
+              <FaCheckCircle size={12} />
+              <span>확인</span>
+            </>
+          ) : (
+            <>
+              <FaRegCheckCircle size={12} />
+              <span>대기</span>
+            </>
+          )}
+        </ButtonValueStyled>
+      </StatusButtonContainer>
+    </>
   );
 };
 
