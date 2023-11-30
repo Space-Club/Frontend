9bf213fd7b4a183492fbbbf6bfffa1803bd35998
SongInJae <pan0663@naver.com>
2023-11-30 17:33:03 +0900

    버그 수정

--- src/apis/event/postApplyEvent.ts
+++ src/apis/event/postApplyEvent.ts
@@ -3,10 +3,10 @@ import { postEventApplyRequest } from '@/types/api/postApplyEvent';
 
 import { axiosClientWithAuth } from '../axiosClient';
 
-const postApplyEvent = async ({ forms = [], ticketCount = 1, eventId }: postEventApplyRequest) => {
+const postApplyEvent = async ({ forms = [], ticketCount, eventId }: postEventApplyRequest) => {
   const data = {
     eventId,
-    ticketCount,
+    ticketCount: ticketCount ?? 1,
     forms,
   };
   await axiosClientWithAuth.post(END_POINTS.EVENT_APPLY, data);
