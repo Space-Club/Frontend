86b5a8bec5c380abbf2ac5cd752e72d7195dc4c5
colorkite10 <colorkite10@gmail.com>
2023-12-01 00:43:07 +0900

    refactor: 클럽 생성 시 비속어, 이미지파일 예외처리, 예외코드에 따른 토스트 메시지

--- src/hooks/query/club/useClub.ts
+++ src/hooks/query/club/useClub.ts
@@ -1,18 +1,26 @@
 import postCreateClub from '@/apis/club/postCreateClub';
 import { ERROR_MESSAGE } from '@/constants/errorMessage';
+import { EXCEPTION_CODE } from '@/constants/exceptionCode';
 import { PATH } from '@/constants/path';
 import { SUCCESS_MESSAGE } from '@/constants/successMessage';
+import useImageException from '@/hooks/useImageException';
+import useTextException from '@/hooks/useTextException';
 import useToast from '@/hooks/useToast';
+import { HttpException } from '@/types/common';
 
 import { useNavigate } from 'react-router-dom';
 
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { AxiosError } from 'axios';
 
 import { QUERY_KEY } from './useClubs';
 
 const useClub = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
+  const { handleTextException } = useTextException();
+  const { handleImageException } = useImageException();
   const { createToast } = useToast();
 
   const { mutate: createClub, isLoading } = useMutation(postCreateClub, {
@@ -22,8 +30,14 @@ const useClub = () => {
       const clubId = data.split('/').pop();
       clubId && navigate(PATH.CLUB.HOME(clubId));
     },
-    onError: () => {
-      createToast({ message: ERROR_MESSAGE.CLUB.CREATE_FAILED, toastType: 'error' });
+    onError: (error: AxiosError<HttpException>) => {
+      handleTextException(error);
+      handleImageException(error);
+      if (
+        !Object.entries(EXCEPTION_CODE).find(([, value]) => value === error.response?.data.code)
+      ) {
+        createToast({ message: ERROR_MESSAGE.CLUB.CREATE_FAILED, toastType: 'error' });
+      }
     },
   });
 
