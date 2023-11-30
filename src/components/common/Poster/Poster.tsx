a0395bf826cee9ebbcf422cc67a8fd4366958909
colorkite10 <colorkite10@gmail.com>
2023-11-30 14:57:51 +0900

    design: 포스터이미지 없을 시 아이콘 띄우기

--- src/components/common/Poster/Poster.tsx
+++ src/components/common/Poster/Poster.tsx
@@ -3,7 +3,7 @@ import { BsFillFileImageFill } from 'react-icons/bs';
 import { PosterAreaStyled, PosterStyled, TagStyled } from './Poster.style';
 
 interface PosterProps {
-  posterSrc?: string;
+  posterSrc?: string | null;
   width?: number;
   isEnded?: boolean;
   children?: React.ReactNode;
@@ -12,8 +12,11 @@ interface PosterProps {
 const Poster = ({ posterSrc, width = 12, isEnded, children }: PosterProps) => {
   return (
     <PosterAreaStyled width={width} isEnded={isEnded}>
-      <PosterStyled src={posterSrc} alt="poster image" />
-      {!posterSrc && <BsFillFileImageFill size={3.5} />}
+      {posterSrc ? (
+        <PosterStyled src={posterSrc} alt="poster image" />
+      ) : (
+        <BsFillFileImageFill size={100} />
+      )}
       <TagStyled>{children}</TagStyled>
     </PosterAreaStyled>
   );
