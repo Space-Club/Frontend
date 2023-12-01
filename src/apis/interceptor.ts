dbc5c8b37b1c6d995e4dd9a594195b63afb278e0
hyesung99 <4538asd@gmail.com>
2023-12-01 18:28:01 +0900

    feat : 토큰 에러 수정

--- src/apis/interceptor.ts
+++ src/apis/interceptor.ts
@@ -35,7 +35,9 @@ const handleRefreshTokenError = (error: AxiosError<HttpException>) => {
 const setToken = (config: InternalAxiosRequestConfig) => {
   const token = getStorage('token');
 
-  config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
   return config;
 };
