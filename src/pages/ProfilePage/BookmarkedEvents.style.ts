e1e1e92a65ee5710a793ade1a6cbaa67e86b6036
hyesung99 <4538asd@gmail.com>
2023-12-03 10:16:05 +0900

    fix : 북마크 레이아웃 스타일 수정

--- src/pages/ProfilePage/BookmarkedEvents.style.ts
+++ src/pages/ProfilePage/BookmarkedEvents.style.ts
@@ -5,43 +5,30 @@ const BookmarkedEventsContainer = styled.div`
   width: 100%;
   display: grid;
   padding: 1.53rem 0;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-gap: 4rem 3rem;
   justify-items: center;
   align-items: center;
 
-  & > div:not(:nth-of-type(-n + 3)):after {
+  & > div:not(:nth-of-type(-n + 2)):after {
     content: '';
     position: absolute;
     width: 90%;
-    height: 100%;
+    height: 1px;
     border-top: 1px solid ${Theme.color.tSeparator};
     top: -2rem;
     right: 5%;
   }
 
-  @media (max-width: 1400px) {
-    grid-template-columns: 1fr 1fr;
-    & > div:not(:nth-of-type(-n + 2)):after {
-      content: '';
-      position: absolute;
-      width: 90%;
-      height: 100%;
-      border-top: 1px solid ${Theme.color.tSeparator};
-      top: -1rem;
-      right: 5%;
-    }
-  }
-
-  @media (max-width: 1040px) {
+  @media (max-width: 800px) {
     grid-template-columns: 1fr;
     & > div:not(:nth-of-type(-n + 1)):after {
       content: '';
       position: absolute;
       width: 90%;
-      height: 100%;
+      height: 1px;
       border-top: 1px solid ${Theme.color.tSeparator};
-      top: -1rem;
+      top: -2rem;
       right: 5%;
     }
   }
