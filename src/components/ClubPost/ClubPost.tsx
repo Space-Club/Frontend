81088f89e4a810ff97271a2b435f1dc77d11fb14
chaeyeon LEE <98521882+colorkite10@users.noreply.github.com>
2023-12-31 13:38:56 +0900

    SKRR-11 design: 클럽 게시글 작성 페이지 디자인 구현 (#243)
    * feat: 글 작성 페이지 구현
    
    * design: 글 작성 페이지 디자인 수정
    
    * fix: 더미값 삭제
    
    * design: 작서완료 버튼 padding 게시판 페이지와 맞춤

--- src/components/ClubPost/ClubPost.tsx
+++ src/components/ClubPost/ClubPost.tsx
@@ -13,23 +13,23 @@ import {
 } from './ClubPost.style';
 
 interface ClubPostProps {
-  clubId?: string;
-  postId?: string;
-  title?: string;
-  content?: string;
-  author?: string;
+  clubId: string;
+  postId: string;
+  title: string;
+  content: string;
+  author: string;
   imageUrl?: string;
-  postDate?: string;
+  postDate: string;
 }
 
 const ClubPost = ({
-  clubId = '1',
-  postId = '1',
-  title = '제목입니다',
-  content = '내용이니다. 이것은 최대 25자로 보여져야 합니다',
-  author = '닉네임',
+  clubId,
+  postId,
+  title,
+  content,
+  author,
   imageUrl,
-  postDate = '2023/12/23',
+  postDate,
 }: ClubPostProps) => {
   const navigate = useNavigate();
   const hasImage = Boolean(imageUrl);
---
+++ src/pages/club/ClubPostWritePage/ClubPostWritePage.style.ts
@@ -0,0 +1,61 @@
+import Theme from '@/styles/Theme';
+import styled from '@emotion/styled';
+
+import { ClubHomePageContainer, ClubHomeTopWrapper } from '../ClubHomePage/ClubHomePage.style';
+
+const ClubPostWritePageContainer = styled(ClubHomePageContainer)``;
+
+const ClubPostWriteTopWrapper = styled(ClubHomeTopWrapper)``;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  width: 100%;
+  padding: 1rem;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  width: 100%;
+`;
+
+const TitleStyled = styled.input`
+  width: 100%;
+  height: 2.5rem;
+  padding: 0.3rem;
+  border: 1px solid ${Theme.color.tSeparator};
+`;
+
+const ContentStyled = styled.textarea`
+  width: 100%;
+  height: 20rem;
+  padding: 0.5rem 0.3rem;
+  border: 1px solid ${Theme.color.tSeparator};
+`;
+
+const FileInputWrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  width: 100%;
+  padding: 0.5rem;
+`;
+
+const ErrorMessageStyled = styled.span`
+  font-size: ${Theme.fontSize.smallContent};
+  color: ${Theme.color.tRed};
+`;
+
+export {
+  ClubPostWritePageContainer,
+  ClubPostWriteTopWrapper,
+  ContentWrapper,
+  ButtonWrapper,
+  TitleStyled,
+  ContentStyled,
+  FileInputWrapper,
+  ErrorMessageStyled,
+};
