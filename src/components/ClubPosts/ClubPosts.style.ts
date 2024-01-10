08bdbfb1f5e303cbfb95c0f2006bc0c80d074a28
colorkite10 <colorkite10@gmail.com>
2024-01-10 16:03:38 +0900

    design: 클럽 게시글 페이지 width 설정

--- src/components/ClubPosts/ClubPosts.style.ts
+++ src/components/ClubPosts/ClubPosts.style.ts
@@ -1,11 +1,8 @@
-import Theme from '@/styles/Theme';
 import styled from '@emotion/styled';
 
 const PostsContainer = styled.div`
-  &:not(:last-of-type)::after {
-    width: 100%;
-    border: 1px solid ${Theme.color.tSeparator};
-  }
+  width: 100%;
+  max-width: 70rem;
 `;
 
 export { PostsContainer };
