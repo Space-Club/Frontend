e202d15797de4f6bd882dd50ca978d4f441724c9
colorkite10 <colorkite10@gmail.com>
2024-01-11 16:09:49 +0900

    feat: 클럽 게시글 댓글 훅 구현

--- src/apis/club/getClubComments.ts
+++ src/apis/club/getClubComments.ts
@@ -1,10 +1,10 @@
 import { END_POINTS } from '@/constants/api';
-import { getClubCommentsRequest, getClubCommentsResponse } from '@/types/api/getClubComments';
+import { GetClubCommentsRequest, GetClubCommentsResponse } from '@/types/api/getClubComments';
 
 import { axiosClientWithAuth } from '../axiosClient';
 
-const getClubComments = async ({ postId, pageNumber }: getClubCommentsRequest) => {
-  const { data } = await axiosClientWithAuth.get<getClubCommentsResponse>(
+const getClubComments = async ({ postId, pageNumber }: GetClubCommentsRequest) => {
+  const { data } = await axiosClientWithAuth.get<GetClubCommentsResponse>(
     END_POINTS.CLUB_COMMENTS(postId, pageNumber),
   );
 
---
+++ src/hooks/query/club/useGetClubCommentsQuery.ts
@@ -0,0 +1,19 @@
+import getClubComments from '@/apis/club/getClubComments';
+import { GetClubCommentsRequest } from '@/types/api/getClubComments';
+
+import { useQuery } from '@tanstack/react-query';
+
+export const QUERY_KEY = { CLUB_COMMENTS: 'CLUB_COMMENTS' };
+
+const useGetClubCommentsQuery = ({ postId, pageNumber }: GetClubCommentsRequest) => {
+  const { data: comments } = useQuery({
+    queryKey: [QUERY_KEY.CLUB_COMMENTS, postId, pageNumber],
+    queryFn: () => getClubComments({ postId, pageNumber }),
+  });
+
+  const { data, sliceData } = comments ?? {};
+
+  return { clubPostComments: data, pageData: sliceData };
+};
+
+export default useGetClubCommentsQuery;
