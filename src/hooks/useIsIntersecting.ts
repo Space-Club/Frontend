13f66cdec16d48ad50ecbd77c9b4fcb93e7a0c90
hyesung99 <4538asd@gmail.com>
2024-01-17 16:50:16 +0900

    comment : 추가한 커스텀 훅 주석 추가

--- src/hooks/useIsIntersecting.ts
+++ src/hooks/useIsIntersecting.ts
@@ -1,5 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 
+// useIsIntersection은 타겟 엘리먼트의 가시성을 검사합니다.
+// @params {HTMLElement} root: 옵션, 타겟의 가시성을 검사하기 위해 뷰포트 대신 사용할 요소 객체, 기본값은 뷰포트입니다.
+// @params {string} rootMargin: Root 범위를 확장하거나 축소하기 위한 마진 값, 기본값은 '0px 0px 0px 0px'입니다.
+// @params {number} threshold: 옵저버 콜백이 실행되는 타겟 엘리먼트의 가시성 비율, 기본값은 0입니다.
+// @returns {boolean} isIntersection: 타겟 엘리먼트가 뷰포트에 들어오면 true를 반환합니다.
+// @returns {RefObject} targetRef: 타겟 엘리먼트의 ref
+
 interface UseIsIntersectionProps {
   root?: HTMLElement;
   rootMargin?: string;
