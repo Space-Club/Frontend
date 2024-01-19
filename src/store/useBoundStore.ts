b904cd915ad1e3f3af5d1470b575fb3509a25043
hyesung99 <4538asd@gmail.com>
2024-01-19 16:16:15 +0900

    feat : zustand BoundStore 생성

---
+++ src/store/useBoundStore.ts
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+import { FormOptionSlice, createFormOptionSlice } from './createFormOptionSlice';
+
+type BoundStore = FormOptionSlice;
+
+export const useBoundStore = create<BoundStore>()((...args) => ({
+  ...createFormOptionSlice(...args),
+}));
