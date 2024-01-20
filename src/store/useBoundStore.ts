0732659b41ba7ef879135422ca5fa912632bea0d
hyesung99 <4538asd@gmail.com>
2024-01-20 17:34:56 +0900

    fix : BoundStore IIFE 오류 수정

--- src/store/useBoundStore.ts
+++ src/store/useBoundStore.ts
@@ -4,6 +4,6 @@ import { FormOptionSlice, createFormOptionSlice } from './createFormOptionSlice'
 
 type BoundStore = FormOptionSlice;
 
-export const useBoundStore = create<BoundStore>()((...args) => ({
+export const useBoundStore = create<BoundStore>((...args) => ({
   ...createFormOptionSlice(...args),
 }));
