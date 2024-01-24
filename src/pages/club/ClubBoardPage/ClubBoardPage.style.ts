3b18ba743b2683155419eacbd72f05f4545aeb9c
colorkite10 <colorkite10@gmail.com>
2024-01-24 15:29:41 +0900

    design: 클럽 게시글 페이지 간격 조정

--- src/pages/club/ClubBoardPage/ClubBoardPage.style.ts
+++ src/pages/club/ClubBoardPage/ClubBoardPage.style.ts
@@ -11,6 +11,7 @@ const ClubBoardContentWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 3rem;
   width: 100%;
   height: 100%;
   padding: 1rem;
